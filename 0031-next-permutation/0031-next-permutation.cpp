bde781f306f2df791b00938a915a76c861192a80
Riya <114819408+notdatriya@users.noreply.github.com>
2023-05-28 12:48:22 +0530

    Time: 4 ms (64.62%), Space: 12.1 MB (67.21%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,28 +1,29 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
+        //2154300 - we will find breakpoint 1 as there is someone greater than 1 on the //right, so isi ko replace karenge, then iterate from the back,find element which is just //greater than this 1, aur breakpoint ke aage ka array hamesha last se increasing hota hai..to
+       // swap karne ke baad reverse kar denge baaki ka array to get the next permutation
         int n=nums.size();
-        int i,j;
-
-        //for getting the index where breakpoint starts
-        //from back, array is increasing
+        int i;
         for(i=n-2;i>=0;i--){
-            if(nums[i]<nums[i+1]){
+            if(nums[i]<nums[i+1]){;
                 break;
             }
         }
-
-            if(i<0){
-                reverse(nums.begin(),nums.end());
+        
+        if(i<0){
+             reverse(nums.begin(),nums.end());
+            return;
+        }
+        
+        for(int j=n-1;j>i;j--){
+            if(nums[j]>nums[i]){
+                swap(nums[j],nums[i]);
+                break;
             }
-            else{
-                for(j=n-1;j>i;j--){
-                    if(nums[j]>nums[i]){
-                        break;
-                    }
-                }
-                swap(nums[i],nums[j]);
-                reverse(nums.begin()+i+1,nums.end());
-            }  
+        }
+        
+          reverse(nums.begin()+i+1,nums.end());
+        
     }
 };
