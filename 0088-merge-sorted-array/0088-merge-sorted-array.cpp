1186148b25a21339baf80e9186aa228fedc6cd48
Riya <114819408+notdatriya@users.noreply.github.com>
2023-05-29 19:07:15 +0530

    Time: 0 ms (100.00%), Space: 9.1 MB (56.42%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,7 +1,59 @@
 class Solution {
 public:
+    
+    void swapgreater(vector<int>&nums1,vector<int>&nums2,int left,int right){
+        if(nums1[left]>nums2[right]){
+            swap(nums1[left],nums2[right]);
+        }
+    }
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        for(int i=0;i<n;i++){
+//      int left=m-1;
+//         int right=0;
+        
+//         while(left>0 && right <n){
+//             if(nums1[left]<nums2[right]){
+//                 swap(nums1[left--],nums2[right++]);
+//             }
+//         }
+        
+//         sort(nums1.begin(),nums1.begin()+m);
+//         sort(nums2.begin(),nums2.begin()+n);
+        
+//         for(int i=0;i<n;i++){
+//             nums1[m+i]=nums2[i];
+//         }
+        
+        //gap method
+        
+//         int length=m+n;
+//         int gap=length/2+length%2;
+//         while (gap>0){
+//             int left=0;
+//             int right=left+gap;
+//             while(left<=right){
+//                 if(left<m && right>=m){
+//                     swapgreater(nums1,nums2,left,right-m);
+//                 }
+                
+//                 else if(left >=m && right>=m){
+//                     swapgreater(nums2,nums2,left-m,right-m);
+//                 }
+//                 else{
+//                     swapgreater(nums1,nums1,left,right);
+//                 }
+//                 left++;right++;
+//             }
+//             if(gap==1){
+//                 break;
+//             }
+//             gap=gap/2+gap%2;
+//         }
+        
+//         for(int i=0;i<n;i++){
+//             nums1[m+i]=nums2[i];
+//         }
+        
+         for(int i=0;i<n;i++){
             nums1[m+i]=nums2[i];
         }
         
