0e0699978d9ec8a94802c6d04e0250ce4cbede13
Riya <114819408+notdatriya@users.noreply.github.com>
2023-06-04 12:27:25 +0530

    Time: 5 ms (53.95%), Space: 10.8 MB (21.99%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -11,35 +11,60 @@
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        ListNode*temp=head;
-        if(head==NULL){
-            return NULL;
-        }
+//         ListNode*temp=head;
+//         if(head==NULL){
+//             return NULL;
+//         }
         
-        int count=0;
-        while(temp!=NULL){
-            count++;
-            temp=temp->next;
-        }
+//         int count=0;
+//         while(temp!=NULL){
+//             count++;
+//             temp=temp->next;
+//         }
         
-        int pos=count-n;
+//         int pos=count-n;
 
-        ListNode*temp2=head;
-        ListNode *prev=NULL;
-        while( pos-- ){
-            prev=temp2;
-            temp2=temp2->next;
+//         ListNode*temp2=head;
+//         ListNode *prev=NULL;
+//         while( pos-- ){
+//             prev=temp2;
+//             temp2=temp2->next;
+//         }
+        
+//         if(prev==NULL){
+//             head=head->next;
+//             return head;
+//         }
+//         ListNode* node2=prev->next;
+//         prev->next=prev->next->next;
+//         delete node2;
+        
+//         return head;
+        
+    //Here time-O(2n) and space-O(1)
+        
+        //optimal approach- use slow and fast poniter
+        //move fast pointer to n steps, now fast will be k-n+1 steps away from the last node
+        //those k-n+1 steps will be covered by slow pointer
+        
+        ListNode* dummy=new ListNode();
+        dummy->next=head;
+        ListNode* slow=dummy;
+        ListNode* fast=dummy;
+        
+        for(int i=1;i<=n;i++){
+            fast=fast->next;
         }
         
-        if(prev==NULL){
-            head=head->next;
-            return head;
+        while(fast->next!=NULL){
+            fast=fast->next;
+            slow=slow->next;
         }
-        ListNode* node2=prev->next;
-        prev->next=prev->next->next;
-        delete node2;
         
-        return head;
+        ListNode* todelete=slow->next;
+        slow->next=slow->next->next;
+        delete todelete;
+        return dummy->next;
         
     }
 };
