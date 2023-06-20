b24e8b1a6726b5a39a21d419e1f2391ee643c608
Riya <114819408+notdatriya@users.noreply.github.com>
2023-06-20 20:56:12 +0530

    Time: 3 ms (68.30%), Space: 7.8 MB (40.82%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,22 +1,47 @@
 class Solution {
 public:
     
-    void p(int index,vector<int>&nums,vector<vector<int>>&ans){
-        if(index==nums.size()){
-            ans.push_back(nums);
+//     void p(int index,vector<int>&nums,vector<vector<int>>&ans){
+//         if(index==nums.size()){
+//             ans.push_back(nums);
+//             return;
+//         }
+        
+//         for(int i=index;i<nums.size();i++){
+//             swap(nums[i],nums[index]);
+//             p(index+1,nums,ans);
+//             swap(nums[i],nums[index]);
+//         }
+//     }
+    
+    //mehtod-2
+    void pp(vector<int>&ds,vector<int>&nums,vector<vector<int>>&ans,int freq[]){
+        if(ds.size()==nums.size()){
+            ans.push_back(ds);
             return;
         }
         
-        for(int i=index;i<nums.size();i++){
-            swap(nums[i],nums[index]);
-            p(index+1,nums,ans);
-            swap(nums[i],nums[index]);
+        for(int i=0;i<nums.size();i++){
+            if(freq[i]==0){
+                ds.push_back(nums[i]);
+                freq[i]=1;
+                pp(ds,nums,ans,freq);
+                freq[i]=0;
+                ds.pop_back();
+            }
         }
     }
     vector<vector<int>> permute(vector<int>& nums) {
+        // vector<vector<int>>ans;
+        // p(0,nums,ans);
+        // return ans;
+        
+        //method-2 -extra space complexity
         vector<vector<int>>ans;
-        int index;
-        p(0,nums,ans);
+        vector<int>ds;
+        int freq[nums.size()];
+        for(int i=0;i<nums.size();i++)freq[i]=0;
+        pp(ds,nums,ans,freq);
         return ans;
     }
 };
