d784ee01891bf48f958a24c2330b585c2d5e1092
Riya <114819408+notdatriya@users.noreply.github.com>
2023-06-29 16:06:06 +0530

    Time: 0 ms (100.00%), Space: 6.9 MB (51.23%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
@@ -1,7 +1,13 @@
 class MyQueue {
 public:
-    stack<int>s1;
-    stack<int>s2;
+    // stack<int>s1;
+    // stack<int>s2;
+    
+    //m-3 optimized- using this, the time complexity of almost all operations will be O(1)
+    //that is called amortised O(1)
+    
+    stack<int>input;
+        stack<int>output;
     MyQueue() {
         
     }
@@ -10,16 +16,20 @@ public:
         // s1.push(x);
         
         //method-2
-         while(!s1.empty()){
-            s2.push(s1.top());
-            s1.pop();
-         }
-        s1.push(x);
-             while(!s2.empty()){
-            s1.push(s2.top());
-            s2.pop();
-        }
+        //  while(!s1.empty()){
+        //     s2.push(s1.top());
+        //     s1.pop();
+        //  }
+        // s1.push(x);
+        //      while(!s2.empty()){
+        //     s1.push(s2.top());
+        //     s2.pop();
+        // }
              
+        
+        //m-3 optimized
+        input.push(x);
+        
     }
     
     int pop() {
@@ -37,9 +47,25 @@ public:
         
         //method-2
         
-        int ans=s1.top();
-        s1.pop();
-        return ans;
+        // int ans=s1.top();
+        // s1.pop();
+        // return ans;
+        
+        //m-3 optimized
+        if(!output.empty()){
+          int ans=output.top();
+            output.pop();
+            return ans;
+        }
+        else{
+            while(!input.empty()){
+                output.push(input.top());
+                input.pop();
+            }
+            int ans=output.top();
+            output.pop();
+            return ans;
+        }
         
     }
     
@@ -57,11 +83,25 @@ public:
         
         //m-2
         
-        return s1.top();
+        // return s1.top();
+        
+        
+        //m-3 optimized
+         if(!output.empty()){
+          return output.top();
+        }
+        else{
+            while(!input.empty()){
+                output.push(input.top());
+                input.pop();
+            }
+            return output.top();
+        }
+        
     }
     
     bool empty() {
-        if(s1.empty()){
+        if(input.empty() && output.empty()){
             return true;
         }
         return false;
