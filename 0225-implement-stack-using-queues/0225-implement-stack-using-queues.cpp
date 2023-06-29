c4641d7f73ee943d3f721559c56c05671e07d85e
Riya <114819408+notdatriya@users.noreply.github.com>
2023-06-29 15:25:16 +0530

    Time: 5 ms (5.49%), Space: 6.8 MB (35.83%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
@@ -1,25 +1,32 @@
 class MyStack {
 public:
+    //commented ,ethod is also right,but we can implement queue using only one queue
     queue<int>q1;
-        queue<int>q2;
+        // queue<int>q2;
     MyStack() {
         
     }
     
     void push(int x) {
-      q2.push(x);
-        while(!q1.empty()){
-            q2.push(q1.front());
+      // q2.push(x);
+      //   while(!q1.empty()){
+      //       q2.push(q1.front());
+      //       q1.pop();
+      //   }
+      //   queue<int>temp;
+      //   temp=q1;
+      //   q1=q2;
+      //   q2=temp;
+        
+        q1.push(x);
+        for(int i=0;i<q1.size()-1;i++){
+            q1.push(q1.front());
             q1.pop();
         }
-        queue<int>temp;
-        temp=q1;
-        q1=q2;
-        q2=temp;
     }
     
     int pop() {
-       int ans= q1.front();
+        int ans=q1.front();
         q1.pop();
         return ans;
     }
