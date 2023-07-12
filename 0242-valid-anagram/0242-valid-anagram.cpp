291a1912f105d0eb9c8aeda51f745b5ccfaf0609
Riya <114819408+notdatriya@users.noreply.github.com>
2023-07-12 15:28:25 +0530

    Time: 0 ms (100.00%), Space: 7.4 MB (40.20%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,11 +1,28 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        if(s==t){
-            return true;
+        // sort(s.begin(),s.end());
+        // sort(t.begin(),t.end());
+        // if(s==t){
+        //     return true;
+        // }
+        // return false;
+        
+        vector<int>freq(26,0);
+        
+        for(char i:s){
+            freq[i- 'a']++;
         }
-        return false;
+        
+        for(char i: t){
+            freq[i - 'a']--;
+        }
+        
+        for(int i:freq){
+            if(i!=0){
+                return false;
+            }
+        }
+        return true;
     }
 };
