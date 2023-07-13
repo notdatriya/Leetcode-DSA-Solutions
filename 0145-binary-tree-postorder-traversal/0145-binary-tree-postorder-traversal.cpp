81dc96ff651bb91606382f954187986b00bc7ab8
Riya <114819408+notdatriya@users.noreply.github.com>
2023-07-13 20:07:59 +0530

    Time: 6 ms (23.17%), Space: 8.3 MB (79.33%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
@@ -12,20 +12,50 @@
 class Solution {
 public:
     vector<int> postorderTraversal(TreeNode* root) {
-        stack<TreeNode*>s1,s2;
+        // stack<TreeNode*>s1,s2;
+        // vector<int>ans;
+        // if(root==NULL)return ans;
+        // s1.push(root);
+        // while(!s1.empty()){
+        //     root=s1.top();
+        //     s1.pop();
+        //     s2.push(root);
+        //     if(root->left!=NULL)s1.push(root->left);
+        //     if(root->right!=NULL)s1.push(root->right);
+        // }
+        // while(!s2.empty()){
+        //     ans.push_back(s2.top()->val);
+        //     s2.pop();
+        // }
+        // return ans;
+        
+        //m-2 with one stack
+        
+        stack<TreeNode*>st;
         vector<int>ans;
-        if(root==NULL)return ans;
-        s1.push(root);
-        while(!s1.empty()){
-            root=s1.top();
-            s1.pop();
-            s2.push(root);
-            if(root->left!=NULL)s1.push(root->left);
-            if(root->right!=NULL)s1.push(root->right);
-        }
-        while(!s2.empty()){
-            ans.push_back(s2.top()->val);
-            s2.pop();
+        if(root==NULL) return ans;
+        TreeNode* curr=root;
+        while(!st.empty() || curr!=NULL){
+            if(curr!=NULL){
+                st.push(curr);
+                curr=curr->left;
+            }
+            else{
+                TreeNode* temp=st.top()->right;
+                if(temp==NULL){
+                    temp=st.top();
+                    st.pop();
+                    ans.push_back(temp->val);
+                    while(!st.empty() && temp==st.top()->right){
+                        temp=st.top();
+                        ans.push_back(temp->val);
+                        st.pop();
+                    }
+                }
+                else{
+                    curr=temp;
+                }
+            }
         }
         return ans;
     }
