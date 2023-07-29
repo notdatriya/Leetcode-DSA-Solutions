a4a877bfb896bda87973dca2ec0a9eacde50f71c
Riya <114819408+notdatriya@users.noreply.github.com>
2023-07-29 13:07:05 +0530

    Time: 4 ms (27.97%), Space: 8.2 MB (84.91%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -12,26 +12,56 @@
 class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
-        stack<TreeNode*>st;
-        vector<int>inorder;
-        TreeNode* node=root;
-        //left-root-right
+//         stack<TreeNode*>st;
+//         vector<int>inorder;
+//         TreeNode* node=root;
+//         //left-root-right
        
-        while(true){
-         if(node!=NULL){
-             st.push(node);
-             node=node->left;
-         }
-            else{
-                if(st.empty())break;
-                node=st.top();
-                st.pop();
-                inorder.push_back(node->val);
-                node=node->right;
-            }
+//         while(true){
+//          if(node!=NULL){
+//              st.push(node);
+//              node=node->left;
+//          }
+//             else{
+//                 if(st.empty())break;
+//                 node=st.top();
+//                 st.pop();
+//                 inorder.push_back(node->val);
+//                 node=node->right;
+//             }
             
+//         }
+//         return inorder;
+        
+        
+        //m-2 morris inorder traversal  time-O(n) and space=O(1)
+        
+        TreeNode* curr=root;
+        vector<int>inorder;
+        while(curr){
+            if(curr->left==NULL){
+                inorder.push_back(curr->val);
+                curr=curr->right;
+            }
+            else{
+                //mark left subtree ka rightmost thread pointing curr root
+                TreeNode* prev=curr->left;
+                while(prev->right!=NULL && prev->right!=curr){
+                    prev=prev->right;
+                }
+                if(prev->right==NULL){
+                    prev->right=curr;
+                    curr=curr->left;
+                }
+                else{
+                    prev->right=NULL;
+                    inorder.push_back(curr->val);
+                    curr=curr->right;
+                }
+            }
         }
         return inorder;
+        
         
     }
 };
