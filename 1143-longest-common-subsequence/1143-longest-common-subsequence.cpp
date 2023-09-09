db0ccf11b0b863d32930591876b7fd616915f973
Riya <114819408+notdatriya@users.noreply.github.com>
2023-09-09 20:13:09 +0530

    Time: 27 ms (91.38%), Space: 6.6 MB (91.21%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -23,30 +23,58 @@ public:
 //         return solve(n-1,m-1,text1,text2,dp);
         
         //Tabular   //index shift so that -1 wala condition satisfy ho sake
+//         int n=text1.size();
+//         int m=text2.size();
+//                 vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        
+//         for(int idx1=0;idx1<=n;idx1++)dp[idx1][0]=0;
+//         for(int idx2=0;idx2<=m;idx2++)dp[0][idx2]=0;
+        
+//         for(int idx1=1;idx1<=n;idx1++){
+//             for(int idx2=1;idx2<=m;idx2++){
+                
+//             if(text1[idx1-1]==text2[idx2-1]){
+                
+//          dp[idx1][idx2]=1+dp[idx1-1][idx2-1] ; //charater match
+//         }
+                
+//         else {
+//             dp[idx1][idx2]= 0+ max(dp[idx1-1][idx2],dp[idx1][idx2-1]);
+//             }
+                
+//             }
+//         }
+        
+    
+//         return dp[n][m];
+
+        
+        //space-optimized
+        
         int n=text1.size();
         int m=text2.size();
-                vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+                vector<int> prev(m+1,0),curr(m+1,0);
         
-        for(int idx1=0;idx1<=n;idx1++)dp[idx1][0]=0;
-        for(int idx2=0;idx2<=m;idx2++)dp[0][idx2]=0;
+        for(int idx2=0;idx2<=m;idx2++) prev[idx2]=0;
         
         for(int idx1=1;idx1<=n;idx1++){
             for(int idx2=1;idx2<=m;idx2++){
                 
             if(text1[idx1-1]==text2[idx2-1]){
                 
-         dp[idx1][idx2]=1+dp[idx1-1][idx2-1] ; //charater match
+         curr[idx2]=1+prev[idx2-1] ; //charater match
         }
                 
         else {
-            dp[idx1][idx2]= 0+ max(dp[idx1-1][idx2],dp[idx1][idx2-1]);
+            curr[idx2]= 0+ max(prev[idx2],curr[idx2-1]);
             }
                 
             }
+        prev=curr;
         }
         
-        return dp[n][m];
-
+        return prev[m];
+        
 
     }
 };
