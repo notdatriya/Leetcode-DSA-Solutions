414ee84329eaaa01d32a384cb488fc49ec64f606
Riya <114819408+notdatriya@users.noreply.github.com>
2023-09-11 13:22:34 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (61.67%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -1,10 +1,24 @@
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
+//         if(s.length()==0) return true;
+//         if(t.length()==0)return false;
+        
+//         if(s[0]==t[0]) return isSubsequence(s.substr(1),t.substr(1));
+//         return isSubsequence(s,t.substr(1));
+        
+        
+        //iterative
         if(s.length()==0) return true;
         if(t.length()==0)return false;
         
-        if(s[0]==t[0]) return isSubsequence(s.substr(1),t.substr(1));
-        return isSubsequence(s,t.substr(1));
+        int s_idx=0;
+    
+        for(int i=0;i<t.length();i++){
+            if(s[s_idx]==t[i])s_idx++;
+            if(s_idx==s.length())return true;
+        }
+        return s_idx==s.length();
     }
+        
 };
