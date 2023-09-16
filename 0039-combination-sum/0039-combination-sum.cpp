4be708253e5c5a51914cb4da4b12c1665382dd88
Riya <114819408+notdatriya@users.noreply.github.com>
2023-09-16 13:08:59 +0530

    Time: 4 ms (75.95%), Space: 11 MB (49.53%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,26 +1,44 @@
 class Solution {
 public:
     
-    void subset(int index,int target,vector<int>&candidates,vector<vector<int>>&ans,vector<int>&ds){
-         if(index==candidates.size()){
-             if(target==0){
-                 ans.push_back(ds);
-             }
-             return;
-         }
+//     void subset(int index,int target,vector<int>&candidates,vector<vector<int>>&ans,vector<int>&ds){
+//          if(index==candidates.size()){
+//              if(target==0){
+//                  ans.push_back(ds);
+//              }
+//              return;
+//          }
     
-      if(candidates[index]<=target){
-          ds.push_back(candidates[index]);
-          subset(index,target-candidates[index],candidates,ans,ds);
-          ds.pop_back();
-      }
-          subset(index+1,target,candidates,ans,ds);
+//       if(candidates[index]<=target){
+//           ds.push_back(candidates[index]);
+//           subset(index,target-candidates[index],candidates,ans,ds);
+//           ds.pop_back();
+//       }
+//           subset(index+1,target,candidates,ans,ds);
         
+//     }
+    
+    void solve(int i,int target,vector<int>&candidates,vector<int>&ds,vector<vector<int>>&ans,int sum){
+        if(i==candidates.size()){
+            if(sum==target){
+                ans.push_back(ds);
+            }
+            return;
+        }
+        
+        if(sum+candidates[i]<=target){
+            ds.push_back(candidates[i]);
+            solve(i,target,candidates,ds,ans,sum+candidates[i]);
+            ds.pop_back();
+        }
+        
+        solve(i+1,target,candidates,ds,ans,sum);
     }
+    
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
          vector<vector<int>>ans;
         vector<int>ds;
-        subset(0,target,candidates,ans,ds);
+        solve(0,target,candidates,ds,ans,0);
         return ans;
     }
 };
