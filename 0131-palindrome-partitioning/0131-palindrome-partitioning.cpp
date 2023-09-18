f52a59f18cc5c741e473614a804b8e6a271e5e89
Riya <114819408+notdatriya@users.noreply.github.com>
2023-09-18 10:30:39 +0530

    Time: 207 ms (10.75%), Space: 145.2 MB (9.31%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,20 +1,37 @@
 class Solution {
 public:
     
-    void p(int index,string s,vector<string>&path ,vector<vector<string>>&ans){
-        if(index==s.size()){
+//     void p(int index,string s,vector<string>&path ,vector<vector<string>>&ans){
+//         if(index==s.size()){
+//             ans.push_back(path);
+//             return;
+//         }
+        
+//         for(int i=index;i<s.size();++i){
+//             if(ispal(s,index,i)){
+//                 path.push_back(s.substr(index,i-index+1));
+//                 p(i+1,s,path,ans);
+//                 path.pop_back();
+//             }
+//         }
+//     }
+    
+    void solve(int idx,vector<string>path,vector<vector<string>>&ans,string s){
+        if(idx==s.size()){
             ans.push_back(path);
             return;
         }
         
-        for(int i=index;i<s.size();++i){
-            if(ispal(s,index,i)){
-                path.push_back(s.substr(index,i-index+1));
-                p(i+1,s,path,ans);
+        for(int i=idx;i<s.size();i++){
+            if(ispal(s,idx,i)){
+                path.push_back(s.substr(idx,i-idx+1));
+                solve(i+1,path,ans,s);
                 path.pop_back();
             }
         }
     }
+    
+    
     
     bool ispal(string s,int start,int end){
         while(start<=end){
@@ -27,7 +44,8 @@ public:
     vector<vector<string>> partition(string s) {
         vector<vector<string>>ans;
         vector<string>path;
-        p(0,s,path,ans);
+        // p(0,s,path,ans);
+        solve(0,path,ans,s);
         return ans;
     }
 };
