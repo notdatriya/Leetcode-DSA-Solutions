99d3a2a4031a93d09c3c88d5985abe69d3b5f0e3
Riya <114819408+notdatriya@users.noreply.github.com>
2023-09-22 16:51:17 +0530

    Time: 0 ms (100.00%), Space: 7.9 MB (33.02%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -15,14 +15,28 @@ public:
         // return solve(n-1,nums,dp);
         
         
-        vector<int>dp(n+1,-1);
-        dp[0]=nums[0];
+        // vector<int>dp(n+1,-1);
+        // dp[0]=nums[0];
+        // for(int i=1;i<n;i++){
+        //     int take=nums[i];
+        //     if(i>1)take+=dp[i-2];
+        //     int nottake=0+dp[i-1];
+        //     dp[i]=max(take,nottake);
+        // }
+        // return dp[n-1];
+        
+        
+        //space optimized
+        int prev=nums[0];
+        int prev2=0;
         for(int i=1;i<n;i++){
-            int take=nums[i];
-            if(i>1)take+=dp[i-2];
-            int nottake=0+dp[i-1];
-            dp[i]=max(take,nottake);
+             int take=nums[i];
+            if(i>1)take+=prev2;
+            int nottake=0+prev;
+            int curri=max(take,nottake);
+            prev2=prev;
+            prev=curri;
         }
-        return dp[n-1];
+        return prev;
     }
 };
