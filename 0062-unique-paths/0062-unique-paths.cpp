c93c3478fdefd007a77819243fd450a533e05085
Riya <114819408+notdatriya@users.noreply.github.com>
2023-09-25 10:22:37 +0530

    Time: 3 ms (28.10%), Space: 7 MB (5.09%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -30,7 +30,28 @@ public:
     }
     int uniquePaths(int m, int n) {
         // return paths(0,0,m,n);
+        // vector<vector<int>>dp(m,vector<int>(n,-1));
+        // return solve(m-1,n-1,m,n,dp);
+        
+        
+        //tabulation
         vector<vector<int>>dp(m,vector<int>(n,-1));
-        return solve(m-1,n-1,m,n,dp);
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0){
+                    dp[i][j]=1;
+                    continue;
+                }
+                int up=0;
+                int left=0;
+                if(i>0) up=dp[i-1][j];
+                if(j>0) left=dp[i][j-1];
+                dp[i][j]=up+left;
+            }
+        }
+        return dp[m-1][n-1];
+        
+        
     }
 };
