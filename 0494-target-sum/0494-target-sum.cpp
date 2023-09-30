0a1532d8f47881c425cda5aa4f0c59967fe46317
Riya <114819408+notdatriya@users.noreply.github.com>
2023-09-30 23:10:27 +0530

    Time: 266 ms (40.74%), Space: 9.4 MB (66.09%) - LeetHub

---
+++ 0494-target-sum/0494-target-sum.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int solve(int i,int target,vector<int>&nums){
+        if(i==0){
+           if(target==0 && nums[0]==0)return 2;
+            if(target==0 || nums[i] == target)return 1;
+            return 0;
+        }
+        
+        int notpick=solve(i-1,target,nums);
+        int pick=0;
+        if(nums[i]<=target){
+            pick=solve(i-1,target-nums[i],nums);
+        }
+        return pick+notpick;
+    }
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int n=nums.size();
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+        }
+        //s1-s2=target   // s2 are subset with minus sign
+        //s1+s2=sum;
+        if(sum-target<0) return 0;
+        if((sum-target)%2)return 0;
+            target=(sum-target)/2;
+            return solve(n-1,target,nums);
+    }
+};
