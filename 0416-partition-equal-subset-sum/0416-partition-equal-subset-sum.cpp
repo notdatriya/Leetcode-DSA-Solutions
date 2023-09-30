c5c8278457db131743c670ab3efb7701e3b110a8
Riya <114819408+notdatriya@users.noreply.github.com>
2023-09-30 11:41:24 +0530

    Time: 565 ms (7.31%), Space: 13.7 MB (63.62%) - LeetHub

---
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    
+    bool solve(int i,int target,vector<int>&nums,vector<vector<int>>&dp){
+        if(target==0)return true;
+        if(i==0){
+            return nums[0]==target;
+        }
+        
+        bool notpick=solve(i-1,target,nums,dp);
+        bool pick=false;
+        if(nums[i]<=target){
+            pick=solve(i-1,target-nums[i],nums,dp);
+        }
+        return dp[i][target]=pick||notpick;
+    }
+        
+    bool canPartition(vector<int>& nums) {
+        
+        int n=nums.size();
+        int totsum=0;
+        for(int i=0;i<n;i++){
+            totsum+=nums[i];
+        }
+         if(totsum%2)return false;
+    vector<vector<bool>>dp(n,vector<bool>((totsum/2)+1,0));
+        // return solve(n-1,totsum/2,nums,dp);
+        
+        
+        //tabulation
+        for(int i=0;i<n;i++)dp[i][0]=true;
+        if(nums[0]<=totsum/2)dp[0][nums[0]]=true;
+        
+        for(int i=1;i<n;i++){
+            for(int target=1;target<=totsum/2;target++){
+                
+                bool notpick=dp[i-1][target];
+                bool pick=false;
+                if(nums[i]<=target){
+                    pick=dp[i-1][target-nums[i]];
+                 }
+                     dp[i][target]=pick||notpick;
+                }
+        }
+        return dp[n-1][totsum/2];
+    }
+};
