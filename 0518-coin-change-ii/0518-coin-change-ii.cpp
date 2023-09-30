281ba40def417f320824a228f9495e5713900cc7
Riya <114819408+notdatriya@users.noreply.github.com>
2023-09-30 23:57:13 +0530

    Time: 43 ms (14.02%), Space: 18.7 MB (28.74%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -18,8 +18,29 @@ public:
     }
     int change(int amount, vector<int>& coins) {
         int n=coins.size();
-        vector<vector<int>>dp(n,vector<int>(amount+1,-1));
-        int ans=solve(n-1,amount,coins,dp);
-        return ans;
+        vector<vector<int>>dp(n,vector<int>(amount+1,0));
+        // int ans=solve(n-1,amount,coins,dp);
+        // return ans;
+        
+        //tabulation
+       for(int target=0;target<=amount;target++){
+           if(target%coins[0]==0) dp[0][target]=1;
+           else if(target==0)dp[0][target]=1;
+           else dp[0][target]=0;
+       }
+        
+        for(int i=1;i<n;i++){
+            for(int target=0;target<=amount;target++){
+                int notpick=dp[i-1][target];
+        int pick=0;
+        if(coins[i]<=target){
+        pick=dp[i][target-coins[i]];
+        }
+        dp[i][target]=pick+notpick;
+            }
+        }
+        return dp[n-1][amount];
+        
+        
     }
 };
