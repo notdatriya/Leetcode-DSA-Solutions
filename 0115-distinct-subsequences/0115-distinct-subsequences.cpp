108deec30bd03fcf0d5cec0831459d530f8fbe90
Riya <114819408+notdatriya@users.noreply.github.com>
2023-10-04 00:12:33 +0530

    Time: 50 ms (22.83%), Space: 6.7 MB (85.57%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -17,20 +17,39 @@ public:
         // vector<vector<int>>dp(n,vector<int>(m,-1));
         // return solve(n-1,m-1,s,t,dp);
         
-        vector<vector<double>>dp(n+1,vector<double>(m+1,0));
+//         vector<vector<double>>dp(n+1,vector<double>(m+1,0));
         
-        for(int i=0;i<=n;i++)dp[i][0]=1;
-        for(int j=1;j<=m;j++)dp[0][j]=0;
+//         for(int i=0;i<=n;i++)dp[i][0]=1;
+//         for(int j=1;j<=m;j++)dp[0][j]=0;
+        
+//         for(int i=1;i<=n;i++){
+//             for(int j=1;j<=m;j++){
+//                 if(s[i-1]==t[j-1]){
+//             dp[i][j]=dp[i-1][j-1]+dp[i-1][j];
+//         }
+//         else dp[i][j]=dp[i-1][j];
+//             }
+//         }
+//         return (int)dp[n][m];
+        
+        //space optimized
+vector<double>prev(m+1,0);
+        vector<double>curr(m+1,0);
+        
+       prev[0]=1;
         
         for(int i=1;i<=n;i++){
+                    curr[0]=1;
             for(int j=1;j<=m;j++){
                 if(s[i-1]==t[j-1]){
-            dp[i][j]=dp[i-1][j-1]+dp[i-1][j];
+            curr[j]=prev[j-1]+prev[j];
         }
-        else dp[i][j]=dp[i-1][j];
+        else curr[j]=prev[j];
             }
+            prev=curr;
         }
-        return (int)dp[n][m];
+        return (int)prev[m];
+        
         
         
     }
