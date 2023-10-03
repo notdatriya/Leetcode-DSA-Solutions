3ba5741e48f806a20129c8d528a1266a1175e87a
Riya <114819408+notdatriya@users.noreply.github.com>
2023-10-04 00:18:10 +0530

    Time: 20 ms (90.39%), Space: 6.7 MB (85.57%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -33,20 +33,35 @@ public:
 //         return (int)dp[n][m];
         
         //space optimized
-vector<double>prev(m+1,0);
-        vector<double>curr(m+1,0);
+// vector<double>prev(m+1,0);
+//         vector<double>curr(m+1,0);
+        
+//        prev[0]=1;
+        
+//         for(int i=1;i<=n;i++){
+//                     curr[0]=1;
+//             for(int j=1;j<=m;j++){
+//                 if(s[i-1]==t[j-1]){
+//             curr[j]=prev[j-1]+prev[j];
+//         }
+//         else curr[j]=prev[j];
+//             }
+//             prev=curr;
+//         }
+//         return (int)prev[m];
+        
+        //1-d array optimized
+        vector<double>prev(m+1,0);
         
        prev[0]=1;
         
         for(int i=1;i<=n;i++){
-                    curr[0]=1;
-            for(int j=1;j<=m;j++){
+            for(int j=m;j>=1;j--){
                 if(s[i-1]==t[j-1]){
-            curr[j]=prev[j-1]+prev[j];
+            prev[j]=prev[j-1]+prev[j];
         }
-        else curr[j]=prev[j];
+        else prev[j]=prev[j];
             }
-            prev=curr;
         }
         return (int)prev[m];
         
