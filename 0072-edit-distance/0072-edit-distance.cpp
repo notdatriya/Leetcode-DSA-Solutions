01a54a247c8a1f604b4f6809f425ee4ec052098b
Riya <114819408+notdatriya@users.noreply.github.com>
2023-10-04 23:30:11 +0530

    Time: 11 ms (59.75%), Space: 6.8 MB (90.73%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -27,53 +27,39 @@ public:
         
         //tabulation
         
-        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
-        for(int i=0;i<=n;i++)dp[i][0]=i;
-        for(int j=1;j<=m;j++)dp[0][j]=j;
-
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=m;j++){
-                if(word1[i-1]==word2[j-1]){
-                    dp[i][j]=dp[i-1][j-1];
-                }
-                else{
-                    dp[i][j]= 1+ min(dp[i-1][j],min(dp[i][j-1],dp[i-1][j-1]));
-                }
-            }
-        }
-        return dp[n][m];
-        //tabulation
-//          vector<vector<int>>dp(n+1,vector<int>(m+1,0));
-    
-//         for(int i=0;i<=n;i++) dp[i][0]=i;
+//         vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+//         for(int i=0;i<=n;i++)dp[i][0]=i;
 //         for(int j=0;j<=m;j++)dp[0][j]=j;
-        
+
 //         for(int i=1;i<=n;i++){
 //             for(int j=1;j<=m;j++){
-//                 if(word1[i-1]==word2[j-1]) dp[i][j]=dp[i-1][j-1];
-//        else dp[i][j]=1+ min(dp[i-1][j],min(dp[i][j-1],dp[i-1][j-1]));
+//                 if(word1[i-1]==word2[j-1]){
+//                     dp[i][j]=dp[i-1][j-1];
+//                 }
+//                 else{
+//                     dp[i][j]= 1+ min(dp[i-1][j],min(dp[i][j-1],dp[i-1][j-1]));
+//                 }
 //             }
 //         }
-        
 //         return dp[n][m];
         
         
         //space opti
         
-//        vector<int>prev(m+1,0),curr(m+1,0);
+       vector<int>prev(m+1,0),curr(m+1,0);
        
-//         for(int j=0;j<=m;j++)prev[j]=j;
+        for(int j=0;j<=m;j++)prev[j]=j;
         
-//         for(int i=1;i<=n;i++){
-//             curr[0]=i;
-//             for(int j=1;j<=m;j++){
-//                 if(word1[i-1]==word2[j-1]) curr[j]=prev[j-1];
-//        else curr[j]=1+ min(prev[j],min(curr[j-1],prev[j-1]));
-//             }
-//             prev=curr;
-//         }
+        for(int i=1;i<=n;i++){
+            curr[0]=i;
+            for(int j=1;j<=m;j++){
+                if(word1[i-1]==word2[j-1]) curr[j]=prev[j-1];
+       else curr[j]=1+ min(prev[j],min(curr[j-1],prev[j-1]));
+            }
+            prev=curr;
+        }
         
-//         return prev[m];
+        return prev[m];
         
         
         
