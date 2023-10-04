28f04d33163a4b43f032465a1caf2a4a72eca9e6
Riya <114819408+notdatriya@users.noreply.github.com>
2023-10-04 20:58:36 +0530

    Time: 86 ms (98.36%), Space: 52.4 MB (81.01%) - LeetHub

--- 0706-design-hashmap/0706-design-hashmap.cpp
+++ 0706-design-hashmap/0706-design-hashmap.cpp
@@ -10,26 +10,69 @@
 // };
 class MyHashMap {
 public:
-    vector<int>arr;
+    // vector<int>arr;
+    vector<vector<pair<int,int>>>arr;
+    int size;
+    int hash(int key){
+            return key%size;
+        }
     MyHashMap() {
-        int size=1e6+1;
+        // int size=1e6+1;
+        // arr.resize(size);
+        // fill(arr.begin(),arr.end(),-1);
+        
+         size=1000;
+        
         arr.resize(size);
-        fill(arr.begin(),arr.end(),-1);
     }
     
     void put(int key, int value) {
-      arr[key]=value;
+      // arr[key]=value;
+        
+        int idx=hash(key);
+        for(auto &it:arr[idx]){
+            if(it.first==key){
+                it.second=value;
+                return;
+            }
+        }
+        arr[idx].push_back({key,value});
         
         
     }
    
     int get(int key) {
-return arr[key];        
+// return arr[key];     
+        
+        int idx=hash(key);
+        for(auto it:arr[idx]){
+            if(it.first==key){
+                return it.second;
+            }
+        }
+        return -1;
     }
     
     void remove(int key) {
         
-        arr[key]=-1;
+        // arr[key]=-1;
+        //  int idx=hash(key);
+        // for(auto it=arr[idx].begin();it!=arr[idx].end();it++){
+        //     if(it->first==key){
+        //         arr.erase(it);
+        //         return;
+        //     }
+        // }
+        
+         int idx = hash(key);
+        auto &bucket = arr[idx];
+        for (auto it = bucket.begin(); it != bucket.end(); ++it) {
+            if (it->first == key) {
+                bucket.erase(it); // Remove the key-value pair if found
+                return;
+            }
+        }
+        
       
     }
 };
