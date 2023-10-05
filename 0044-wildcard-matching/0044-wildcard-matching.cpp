c3bba57b9df3e7cde6d99654f4cd8630e3d57b98
Riya <114819408+notdatriya@users.noreply.github.com>
2023-10-05 11:39:36 +0530

    Time: 115 ms (13.17%), Space: 11.7 MB (64.19%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -22,7 +22,36 @@ public:
     bool isMatch(string s, string p) {
         int n=s.size();
         int m=p.size();
-        vector<vector<int>>dp(n,vector<int>(m,-1));
-        return solve(n-1,m-1,s,p,dp);
+        // vector<vector<int>>dp(n,vector<int>(m,-1));
+        // return solve(n-1,m-1,s,p,dp);
+        
+        //tabulation
+        vector<vector<bool>>dp(n+1,vector<bool>(m+1,false));
+        dp[0][0]=true;
+        for(int i=1;i<=n;i++)dp[i][0]=false;
+        for(int j=1;j<=m;j++){
+            bool flag=true;
+            for(int it=1;it<=j;it++){
+                if(p[it-1]!='*'){
+                    flag=false;
+                    break;
+                }
+            }
+            dp[0][j]=flag;
+        }
+        
+        
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s[i-1]==p[j-1] || p[j-1]=='?'){
+                dp[i][j]=dp[i-1][j-1];
+        }
+        else if(p[j-1]=='*'){
+            dp[i][j]=dp[i-1][j] || dp[i][j-1];
+        }
+        else dp[i][j]=false;
+            }
+        }
+        return dp[n][m];
     }
 };
