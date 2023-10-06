37f0b379af293b4bedcb19ab171a3fb2b3785b4c
Riya <114819408+notdatriya@users.noreply.github.com>
2023-10-06 12:07:05 +0530

    Time: 12 ms (15.48%), Space: 15.5 MB (21.22%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -25,9 +25,9 @@ public:
         dp[n][0]=0;
         dp[n][1]=0;
         
-        int profit=0;
         for(int i=n-1;i>=0;i--){
             for(int buy=0 ;buy<=1;buy++){
+                        int profit=0;
                 if(buy){
             profit= max(-prices[i] +dp[i+1][0],0+ dp[i+1][1]);
         }
