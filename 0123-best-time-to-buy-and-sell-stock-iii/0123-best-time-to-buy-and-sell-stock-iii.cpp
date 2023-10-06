a9e431ae6153c0d1f133ee9f89dcda02b80123c4
Riya <114819408+notdatriya@users.noreply.github.com>
2023-10-07 00:50:02 +0530

    Time: 179 ms (66.71%), Space: 75.5 MB (82.70%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -15,38 +15,74 @@ public:
     }
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
-        vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(2,vector<int>(3,0)));
+        // vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(2,vector<int>(3,0)));
         // return solve(0,1,2,prices,n,dp);
         
         //tabulation
         
-        for(int buy=0;buy<=1;buy++){
-            for(int cap=2;cap>=0;cap--){
-                dp[n][buy][cap]=0;
-            }
-        }
+//         for(int buy=0;buy<=1;buy++){
+//             for(int cap=2;cap>=0;cap--){
+//                 dp[n][buy][cap]=0;
+//             }
+//         }
         
-        for(int i=0;i<n;i++){
-            for(int buy=1;buy>=0;buy--){
-                dp[i][buy][0]=0;
-            }
-        }
+//         for(int i=0;i<n;i++){
+//             for(int buy=1;buy>=0;buy--){
+//                 dp[i][buy][0]=0;
+//             }
+//         }
         
+//         for(int i=n-1;i>=0;i--){
+//             for(int buy=0;buy<=1;buy++){
+//                 for(int cap=1;cap<=2;cap++){
+//                     int profit=0;
+//                 if(buy){
+//                 profit= max(-prices[i]+ dp[i+1][0][cap],0 + dp[i+1][1][cap]);
+//                 }
+//                 else{
+//                 profit=max(prices[i] + dp[i+1][1][cap-1],0 + dp[i+1][0][cap]);
+//                     }           
+//          dp[i][buy][cap]=profit;
+//                 }
+//             }
+//         }
+//         return dp[0][1][2];
+        
+        
+        //space-optimized
+//          for(int buy=0;buy<=1;buy++){
+//             for(int cap=2;cap>=0;cap--){
+//                 dp[n][buy][cap]=0;
+//             }
+//         }
+        
+//         for(int i=0;i<n;i++){
+//             for(int buy=1;buy>=0;buy--){
+//                 dp[i][buy][0]=0;
+//             }
+//         }
+        
+        //no need as already initialized to 0
+        
+        vector<vector<int>>ahead(2,vector<int>(3,0));
+        vector<vector<int>>curr(2,vector<int>(3,0));
+
         for(int i=n-1;i>=0;i--){
             for(int buy=0;buy<=1;buy++){
                 for(int cap=1;cap<=2;cap++){
                     int profit=0;
                 if(buy){
-                profit= max(-prices[i]+ dp[i+1][0][cap],0 + dp[i+1][1][cap]);
+                profit= max(-prices[i]+ ahead[0][cap],0 + ahead[1][cap]);
                 }
                 else{
-                profit=max(prices[i] + dp[i+1][1][cap-1],0 + dp[i+1][0][cap]);
+                profit=max(prices[i] + ahead[1][cap-1],0 + ahead[0][cap]);
                     }           
-         dp[i][buy][cap]=profit;
+         curr[buy][cap]=profit;
                 }
             }
+            ahead=curr;
         }
-        return dp[0][1][2];
+        return ahead[1][2];
         
         
     }
