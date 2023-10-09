459ab52dec5045a746fd239386c137a93b75ee70
Riya <114819408+notdatriya@users.noreply.github.com>
2023-10-09 11:38:25 +0530

    Time: 499 ms (30.36%), Space: 292.5 MB (11.24%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int solve(int idx,int prev_idx,vector<int>&nums,vector<vector<int>>&dp){
+    // int solve(int idx,int prev_idx,vector<int>&nums,vector<vector<int>>&dp){
         //Recursion
 //         int n=nums.size();
 //         if(idx==n)return 0;
@@ -16,27 +16,47 @@ public:
         
         
         //memoization
-                  int n=nums.size();
+//                   int n=nums.size();
 
-        if(idx==n)return 0;
-        if(dp[idx][prev_idx+1]!=-1) return dp[idx][prev_idx+1];
-        int len =0+ solve(idx+1,prev_idx,nums,dp);   //not take
+//         if(idx==n)return 0;
+//         if(dp[idx][prev_idx+1]!=-1) return dp[idx][prev_idx+1];
+//         int len =0+ solve(idx+1,prev_idx,nums,dp);   //not take
         
-        if(prev_idx==-1 || nums[idx]>nums[prev_idx]){
-            len=max(len,1+ solve(idx+1,idx,nums,dp));
-        }
+//         if(prev_idx==-1 || nums[idx]>nums[prev_idx]){
+//             len=max(len,1+ solve(idx+1,idx,nums,dp));
+//         }
         
-        return dp[idx][prev_idx+1]=len;
+//         return dp[idx][prev_idx+1]=len;
         
+    // }
         
+      
         
-    }
+    // }
+    
+    
     int lengthOfLIS(vector<int>& nums) {
         int idx=0;
         int prev_idx=-1;
           int n=nums.size();
-        vector<vector<int>>dp(n,vector<int>(n+1,-1)) ; // idx - 0 to n-1 and prev_idx is from -1 to n-1 so we will store by +1;
-        return solve(idx,prev_idx,nums,dp);
+        // vector<vector<int>>dp(n,vector<int>(n+1,-1)) ; // idx - 0 to n-1 and prev_idx is from -1 to n-1 so we will store by +1;
+        // return solve(idx,prev_idx,nums,n);
         
+        
+        
+         //Tabulation
+        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+        for(int idx=n-1;idx>=0;idx--){
+            for(int prev_idx=idx-1;prev_idx>=-1;prev_idx--){
+                int len =0+ dp[idx+1][prev_idx+1];   //not take
+        
+        if(prev_idx==-1 || nums[idx]>nums[prev_idx]){
+            len=max(len,1+ dp[idx+1][idx+1]);
+        }
+        
+        dp[idx][prev_idx+1]=len;
+            }
+        }
+        return dp[0][-1+1];
     }
 };
