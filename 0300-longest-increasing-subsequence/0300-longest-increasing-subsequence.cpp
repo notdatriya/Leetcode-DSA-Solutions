571c6429c86ea3a2ae64c6a7d1a32f1cdfa00d5b
Riya <114819408+notdatriya@users.noreply.github.com>
2023-10-09 11:40:51 +0530

    Time: 328 ms (36.49%), Space: 11 MB (39.34%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -45,18 +45,36 @@ public:
         
         
          //Tabulation
-        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+//         vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+//         for(int idx=n-1;idx>=0;idx--){
+//             for(int prev_idx=idx-1;prev_idx>=-1;prev_idx--){
+//                 int len =0+ dp[idx+1][prev_idx+1];   //not take
+        
+//         if(prev_idx==-1 || nums[idx]>nums[prev_idx]){
+//             len=max(len,1+ dp[idx+1][idx+1]);
+//         }
+        
+//         dp[idx][prev_idx+1]=len;
+//             }
+//         }
+//         return dp[0][-1+1];
+        
+        //space -optimized
+        vector<int>after(n+1,0);
+        vector<int>curr(n+1,0);
+        
         for(int idx=n-1;idx>=0;idx--){
             for(int prev_idx=idx-1;prev_idx>=-1;prev_idx--){
-                int len =0+ dp[idx+1][prev_idx+1];   //not take
+                int len =0+ after[prev_idx+1];   //not take
         
         if(prev_idx==-1 || nums[idx]>nums[prev_idx]){
-            len=max(len,1+ dp[idx+1][idx+1]);
+            len=max(len,1+ after[idx+1]);
         }
         
-        dp[idx][prev_idx+1]=len;
+        curr[prev_idx+1]=len;
             }
+            after=curr;
         }
-        return dp[0][-1+1];
+        return after[-1+1];
     }
 };
