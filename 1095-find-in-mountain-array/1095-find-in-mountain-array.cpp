c659d9f45bb67ab8820a671e91b75acacfed2ae4
Riya <114819408+notdatriya@users.noreply.github.com>
2023-10-12 14:56:56 +0530

    Time: 3 ms (78.78%), Space: 7.4 MB (14.97%) - LeetHub

--- 1095-find-in-mountain-array/1095-find-in-mountain-array.cpp
+++ 1095-find-in-mountain-array/1095-find-in-mountain-array.cpp
@@ -10,104 +10,104 @@
 
 class Solution {
 public:
-//     int bs(int n,MountainArray &mountainArr){
-//         int l=0;int h=n-1;
-//         while(l<=h){
-//             int mid=l+(h-l)/2;
-//             if(mountainArr.get(mid)<mountainArr.get(mid+1)){
-//                 l=mid+1;
-//             }
-//             else{
-//                 h=mid-1;
-//             }
-//         }
-//         return l;
-//     }
+    int bs(int n,MountainArray &mountainArr){
+        int l=0;int h=n-1;
+        while(l<=h){
+            int mid=l+(h-l)/2;
+            if(mountainArr.get(mid)<mountainArr.get(mid+1)){
+                l=mid+1;
+            }
+            else{
+                h=mid-1;
+            }
+        }
+        return l;
+    }
     
-//     int solve(int l,int h,bool left,int target,int n,MountainArray &mountainArr){
-//         while(l<=h){
-//             int mid=l+(h-l)/2;
+    int solve(int l,int h,bool left,int target,int n,MountainArray &mountainArr){
+        while(l<=h){
+            int mid=l+(h-l)/2;
             
-//             if(left){
-//                 if(mountainArr.get(mid)==target){
-//                 return mid;
-//                 }
+            if(left){
+                if(mountainArr.get(mid)==target){
+                return mid;
+                }
             
-//             else if(mountainArr.get(mid)<target){
-//                 l=mid+1;
-//             }
-//             else{
-//                 h=mid-1;
-//             }
-//             }
+            else if(mountainArr.get(mid)<target){
+                l=mid+1;
+            }
+            else{
+                h=mid-1;
+            }
+            }
             
-//             else{
-//                  if(mountainArr.get(mid)==target){
-//                 return mid;
-//                 }
+            else{
+                 if(mountainArr.get(mid)==target){
+                return mid;
+                }
             
-//             else if(mountainArr.get(mid)<target){
-//                     h=mid-1;
-//                 }
-//             else{
-//                     l=mid+1;
-//                 }
+            else if(mountainArr.get(mid)<target){
+                    h=mid-1;
+                }
+            else{
+                    l=mid+1;
+                }
+            }
+        }
+        return -1;
+    }
+    int findInMountainArray(int target, MountainArray &mountainArr) {
+        int n=mountainArr.length();
+        int peak=bs(n,mountainArr);
+        int idx1=solve(0,peak,true,target,n,mountainArr);
+        if(idx1!=-1)return idx1;
+        int idx2=solve(peak+1,n-1,false,target,n,mountainArr);
+        if(idx2!=-1)return idx2;
+         return -1;
+        
+        
+        
+//          int n = mountainArr.length();
+//         int l = 0, r = n - 1;
+
+//         while (l <= r) {
+//             int mid = l + (r - l) / 2;
+//             if (mountainArr.get(mid) < mountainArr.get(mid + 1)) {
+//                 l = mid + 1;
+//             } else {
+//                 r = mid - 1;
 //             }
 //         }
+//         int peak = l;
+
+//         l = 0, r = peak;
+//         while (l <= r) {
+//             int mid = l + (r - l) / 2;
+//             int middle = mountainArr.get(mid);
+//             if (middle == target) {
+//                 return mid;
+//             } else if (middle < target) {
+//                 l = mid + 1;
+//             } else {
+//                 r = mid - 1;
+//             }
+//         }
+
+//         l = peak + 1;
+//         r = n - 1;
+//         while (l <= r) {
+//             int mid = l + (r - l) / 2;
+//             int middle = mountainArr.get(mid);
+//             if (middle == target) {
+//                 return mid;
+//             } else if (middle < target) {
+//                 r = mid - 1;
+//             } else {
+//                 l = mid + 1;
+//             }
+//         }
+
 //         return -1;
-//     }
-    int findInMountainArray(int target, MountainArray &mountainArr) {
-       //  int n=mountainArr.length();
-       //  int peak=bs(n,mountainArr);
-       //  int idx1=solve(0,peak,true,target,n,mountainArr);
-       //  if(mountainArr.get(idx1)==target)return idx1;
-       //  int idx2=solve(peak+1,n-1,false,target,n,mountainArr);
-       // if(mountainArr.get(idx2)==target)return idx2;
-       //  return -1;
-        
-        
-        
-         int n = mountainArr.length();
-        int l = 0, r = n - 1;
-
-        while (l <= r) {
-            int mid = l + (r - l) / 2;
-            if (mountainArr.get(mid) < mountainArr.get(mid + 1)) {
-                l = mid + 1;
-            } else {
-                r = mid - 1;
-            }
-        }
-        int peak = l;
-
-        l = 0, r = peak;
-        while (l <= r) {
-            int mid = l + (r - l) / 2;
-            int middle = mountainArr.get(mid);
-            if (middle == target) {
-                return mid;
-            } else if (middle < target) {
-                l = mid + 1;
-            } else {
-                r = mid - 1;
-            }
-        }
-
-        l = peak + 1;
-        r = n - 1;
-        while (l <= r) {
-            int mid = l + (r - l) / 2;
-            int middle = mountainArr.get(mid);
-            if (middle == target) {
-                return mid;
-            } else if (middle < target) {
-                r = mid - 1;
-            } else {
-                l = mid + 1;
-            }
-        }
-
-        return -1;
         
     }
 };
