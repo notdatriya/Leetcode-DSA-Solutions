e920d56ee0f195e27364d01a17e842988c8172ec
Riya <114819408+notdatriya@users.noreply.github.com>
2023-10-15 11:44:18 +0530

    Time: 147 ms (71.34%), Space: 115.7 MB (53.23%) - LeetHub

--- 2742-painting-the-walls/2742-painting-the-walls.cpp
+++ 2742-painting-the-walls/2742-painting-the-walls.cpp
@@ -1,25 +1,18 @@
 class Solution {
 public:
-     int solve(vector<int>& cost, vector<int>& time, int i, int bank,vector<vector<int>> &dp){
-        if(bank <= 0){
-            return 0;
-        }
-        if(i >= cost.size()){
-            return 1e9;   
-        }
-        if(dp[i][bank] != -1){
-            return dp[i][bank];
-        }
-        else {
-            // take or not take options
-            int nottake = solve(cost, time, i+1, bank,dp);
-            int take = cost[i] + solve(cost, time, i+1, bank -  time[i] -1,dp); 
-            return dp[i][bank] = min(nottake, take);      
-        }
+     
+    int solve(int i,int rem,vector<int>&cost,vector<int>&time,vector<vector<int>>&dp){
+        //if rem walls becomes zero,that means free wale ne paint kardi, to cost zero
+        if(rem<=0)return 0;
+        if(i<0)return 1e9;
+        if(dp[i][rem]!=-1)return dp[i][rem];
+        int notpick= solve(i-1,rem,cost,time,dp);
+        int pick= cost[i] + solve(i-1,rem-1-time[i],cost,time,dp);
+        return dp[i][rem]=min(pick,notpick);
     }
     int paintWalls(vector<int>& cost, vector<int>& time) {
         int n=cost.size();
-        vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
-        return solve(cost, time, 0, time.size(),dp);
+        vector<vector<int>> dp(n,vector<int>(n+1,-1));
+        return solve(n-1,n,cost, time,dp);
     }
 };
