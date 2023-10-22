4261b1f01c9d5468cfe9fd055b7eabce413a535c
Riya <114819408+notdatriya@users.noreply.github.com>
2023-10-23 01:59:08 +0530

    Time: 12 ms (94.27%), Space: 14.5 MB (37.99%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,38 +1,71 @@
 class Solution {
 public:
     
-    bool dfs(vector<vector<int>>& adj,int vis[],int pathvis[],int i){
-        vis[i]=1;
-        pathvis[i]=1;
-        for(auto it:adj[i]){
-            if(!vis[it]){
-                if(dfs(adj,vis,pathvis,it)==true)return true;
-            }
+//     bool dfs(vector<vector<int>>& adj,int vis[],int pathvis[],int i){
+//         vis[i]=1;
+//         pathvis[i]=1;
+//         for(auto it:adj[i]){
+//             if(!vis[it]){
+//                 if(dfs(adj,vis,pathvis,it)==true)return true;
+//             }
             
-            else if(pathvis[it]){
-                return true;
-            }
-        }
-        pathvis[i]=0;
-        return false;
-    }
+//             else if(pathvis[it]){
+//                 return true;
+//             }
+//         }
+//         pathvis[i]=0;
+//         return false;
+//     }
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         vector<vector<int>> adj(numCourses);
         for(auto num : prerequisites){
-            adj[num[0]].push_back(num[1]);
+            adj[num[1]].push_back(num[0]);
         }
-        int vis[numCourses];
-        int pathvis[numCourses];
-        for(int i=0;i<numCourses;i++){
-            vis[i]=0;
-            pathvis[i]=0;
-        }
+//         int vis[numCourses];
+//         int pathvis[numCourses];
+//         for(int i=0;i<numCourses;i++){
+//             vis[i]=0;
+//             pathvis[i]=0;
+//         }
         
-        for(int i=0;i<numCourses;i++){
-            if(!vis[i]){
-                if(dfs(adj,vis,pathvis,i)==true)return false;
+//         for(int i=0;i<numCourses;i++){
+//             if(!vis[i]){
+//                 if(dfs(adj,vis,pathvis,i)==true)return false;
+//             }
+//         }
+//         return true;
+        
+        
+        //topo
+        vector<int>indeg(numCourses,0);
+    for(int i=0;i<numCourses;i++){
+        for(auto it:adj[i]){
+            indeg[it]++;
+        }
+    }
+
+    queue<int>q;
+    for(int i=0;i<numCourses;i++){
+        if(indeg[i]==0){
+            q.push(i);
+        }
+    }
+    // vector<int>ans;
+int c=0;
+    while(!q.empty()){
+        int ele=q.front();
+        q.pop();
+        // ans.push_back(ele);
+        c++;
+        for(auto it:adj[ele]){
+            indeg[it]--;
+            if(indeg[it]==0){
+                q.push(it);
             }
         }
-        return true;
+
+    }
+    return c==numCourses;
+
     }
 };
