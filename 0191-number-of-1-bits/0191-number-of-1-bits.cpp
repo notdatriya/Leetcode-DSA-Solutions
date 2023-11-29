13603e5841789fd6cb87d94209951f5abc34f39e
Riya <114819408+notdatriya@users.noreply.github.com>
2023-11-29 20:56:58 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (9.00%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -3,7 +3,7 @@ public:
     int hammingWeight(uint32_t n) {
         uint32_t count=0;
          uint32_t mask=1;
-        while(n){
+        for(int i=0;i<32;i++){
         if(n & mask)count++;
             n=n>>1;
             
