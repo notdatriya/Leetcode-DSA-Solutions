ee7955505b2bfe73735fca764577fb1f39063683
Riya <114819408+notdatriya@users.noreply.github.com>
2023-12-02 15:36:56 +0530

    Time: 119 ms (43.34%), Space: 49 MB (36.60%) - LeetHub

--- 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.cpp
+++ 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.cpp
@@ -1,14 +1,41 @@
 class Solution {
 public:
     int countCharacters(vector<string>& words, string chars) {
+//         int ans=0;
+//         for(auto word:words){
+//            string temp=chars;
+//             bool valid=true;
+//             for(char c: word){
+//                 auto it=find(temp.begin(),temp.end(),c);
+//                 if(it!=temp.end()){
+//                     temp.erase(it);
+//                 }
+//                 else{
+//                     valid=false;
+//                     break;
+//                 }
+//             }
+//             if(valid){
+//                 ans+=word.length();
+//             }
+            
+//         }
+//         return ans;
+        
+        
+        //map
+        unordered_map<char,int>mpp;
+        for(char c:chars){
+            mpp[c]++;
+        }
+        
         int ans=0;
         for(auto word:words){
-           string temp=chars;
+            unordered_map<char,int>temp=mpp;
             bool valid=true;
-            for(char c: word){
-                auto it=find(temp.begin(),temp.end(),c);
-                if(it!=temp.end()){
-                    temp.erase(it);
+            for(char c:word){
+                if(temp.find(c)!=temp.end() && temp[c] > 0){
+                    temp[c]--;
                 }
                 else{
                     valid=false;
@@ -18,7 +45,6 @@ public:
             if(valid){
                 ans+=word.length();
             }
-            
         }
         return ans;
       
