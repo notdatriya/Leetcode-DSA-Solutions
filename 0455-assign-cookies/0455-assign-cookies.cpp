c3b293896d75e4ebca1c231c05d63f28f95fc682
Riya <114819408+notdatriya@users.noreply.github.com>
2024-01-01 20:43:14 +0530

    Time: 50 ms (7.86%), Space: 17.8 MB (36.11%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.cpp
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -4,19 +4,31 @@ public:
         sort(g.begin(),g.end());
         sort(s.begin(),s.end());
         
-        map<int,int>mpp;
-        for(auto it:s){
-            mpp[it]++;
-        }
+        // map<int,int>mpp;
+        // for(auto it:s){
+        //     mpp[it]++;
+        // }
+        // int count=0;
+        // for(auto i:g){
+        //     for(auto j :s){
+        //         if(j>=i && mpp[j]>0){
+        //             count++;
+        //             mpp[j]--;
+        //             break;
+        //         }
+        //     }
+        // }
+        // return count;
+        
+        int j=0;
         int count=0;
-        for(auto i:g){
-            for(auto j :s){
-                if(j>=i && mpp[j]>0){
-                    count++;
-                    mpp[j]--;
-                    break;
-                }
+        int i=0;
+        while(j<s.size() && i<g.size()){
+            if(s[j]>=g[i]){
+                count++;
+                i++;
             }
+            j++;
         }
         return count;
     }
