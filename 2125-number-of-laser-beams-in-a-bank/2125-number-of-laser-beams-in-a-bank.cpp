8b910df0a1faf95c6ff30d42f9f4e3935db463f4
Riya <114819408+notdatriya@users.noreply.github.com>
2024-01-03 22:21:49 +0530

    Time: 90 ms (57.48%), Space: 29.1 MB (29.62%) - LeetHub

--- 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.cpp
+++ 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.cpp
@@ -1,27 +1,44 @@
 class Solution {
 public:
     int numberOfBeams(vector<string>& bank) {
-        map<int,int>mpp;
+//         map<int,int>mpp;
         
-        int i=0;
-        for(auto s:bank){
-            for(char c:s){
-                if(c=='1'){
-                    mpp[i]++;
-                }
-            }
-            i++;
-        }
+//         int i=0;
+//         for(auto s:bank){
+//             for(char c:s){
+//                 if(c=='1'){
+//                     mpp[i]++;
+//                 }
+//             }
+//             i++;
+//         }
         
+//         int ans=0;
+//         for(auto it:mpp){
+//             for(int i=it.first-1;i>=0;i--){
+//                 int p=mpp[i]*mpp[it.first];
+//                 if(p!=0){
+//                     ans+=p;
+//                     break;
+//                 }
+//             }
+//         }
+//         return ans;
+        
+        
+        
+        
+        //m-2
+        int prevcount=0;
         int ans=0;
-        for(auto it:mpp){
-            for(int i=it.first-1;i>=0;i--){
-                int p=mpp[i]*mpp[it.first];
-                if(p!=0){
-                    ans+=p;
-                    break;
-                }
+        for(string s:bank){
+            int currcount=0;
+            for(char c:s){
+                if(c=='1')currcount++;
             }
+            ans+=currcount*prevcount;
+            if(currcount==0)prevcount=prevcount;
+            else prevcount=currcount;
         }
         return ans;
     }
