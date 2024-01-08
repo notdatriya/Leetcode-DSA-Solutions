df6e958292fd34247465cb3e032df46f69b5b7bb
Riya <114819408+notdatriya@users.noreply.github.com>
2024-01-08 20:15:28 +0530

    Time: 103 ms (39.43%), Space: 65.1 MB (14.45%) - LeetHub

--- 0938-range-sum-of-bst/0938-range-sum-of-bst.cpp
+++ 0938-range-sum-of-bst/0938-range-sum-of-bst.cpp
@@ -11,19 +11,29 @@
  */
 class Solution {
 public:
-    void solve(TreeNode* root,int low,int high,int &sum){
-        if(root==NULL)return ;
-        if(root->val>=low && root->val<=high){
-            sum+=root->val;
-        }
-       solve(root->left,low,high,sum);
-        solve(root->right,low,high,sum);
+//     void solve(TreeNode* root,int low,int high,int &sum){
+//         if(root==NULL)return ;
+//         if(root->val>=low && root->val<=high){
+//             sum+=root->val;
+//         }
+//        solve(root->left,low,high,sum);
+//         solve(root->right,low,high,sum);
         
+//     }
+    
+    int solve(TreeNode* root, int low,int high){
+        if(root==NULL)return 0;
+        int currvalue=0;
+         if(root->val>=low && root->val<=high){
+            currvalue=root->val;
+        }
+        return currvalue+solve(root->left,low,high)+solve(root->right,low,high);
     }
     int rangeSumBST(TreeNode* root, int low, int high) {
-        int sum=0;
-     solve(root,low,high,sum);
-        return sum;
+        // int sum=0;
+     // solve(root,low,high,sum);
+        // return sum;
+        return solve(root,low,high);
         
     }
 };
