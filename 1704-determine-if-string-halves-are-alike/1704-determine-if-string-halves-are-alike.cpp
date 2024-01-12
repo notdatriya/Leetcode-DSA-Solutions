aec9b3d8f9ffb85441e6d759ba64e6a6300c60e7
Riya <114819408+notdatriya@users.noreply.github.com>
2024-01-12 22:30:18 +0530

    Time: 5 ms (31.19%), Space: 7 MB (72.68%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.cpp
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.cpp
@@ -4,18 +4,23 @@ public:
         string str = "aeiouAEIOU";
         if(s.length()%2)return false;
         int count1=0;
+        int count2=0;
+
+        int mid=s.length()/2;
         for(int i=0;i<s.length()/2;i++){
             if(str.find(s[i]) != string::npos){
                 count1++;
             }
-        }
-        
-         int count2=0;
-        for(int i=s.length()/2;i<s.length();i++){
-            if(str.find(s[i]) != string::npos){
+            if(str.find(s[i+mid]) != string::npos){
                 count2++;
             }
         }
+        
+        // for(int i=s.length()/2;i<s.length();i++){
+        //     if(str.find(s[i]) != string::npos){
+        //         count2++;
+        //     }
+        // }
         
         if(count1==count2)return true;
         else return false;
