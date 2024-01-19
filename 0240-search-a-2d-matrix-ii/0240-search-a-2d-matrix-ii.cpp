c12c21437e3bf54c435f0fb93ff51fbb632bbaf2
Riya <114819408+notdatriya@users.noreply.github.com>
2024-01-19 16:55:16 +0530

    Time: 36 ms (99.25%), Space: 17.4 MB (17.30%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -1,27 +1,45 @@
 class Solution {
 public:
     
-     bool bs(vector<int>&arr,int n,int target){
-        int low=0;
-        int high=n-1;
-        while(low<=high){
-            int mid=low+(high-low)/2;
+//      bool bs(vector<int>&arr,int n,int target){
+//         int low=0;
+//         int high=n-1;
+//         while(low<=high){
+//             int mid=low+(high-low)/2;
             
-            if(arr[mid]==target)return true;
-            else if(arr[mid]>target){
-                high=mid-1;
-            }
-            else{
-                low=mid+1;
-            }
-        }
-        return false;
-    }
+//             if(arr[mid]==target)return true;
+//             else if(arr[mid]>target){
+//                 high=mid-1;
+//             }
+//             else{
+//                 low=mid+1;
+//             }
+//         }
+//         return false;
+//     }
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int n=matrix.size();
         int m=matrix[0].size();
-        for(int i=0;i<n;i++){
-            if(bs(matrix[i],m,target)==true)return true;
+        // for(int i=0;i<n;i++){
+        //     if(bs(matrix[i],m,target)==true)return true;
+        // }
+        // return false;
+        
+        
+        //m-2 most optimal
+          if (matrix.empty() || matrix[0].empty()) {
+        return false; // Empty matrix
+          }
+        int row=0;
+        int col=m-1;
+        
+        while(row<n && col>=0){
+            if(matrix[row][col]==target)return true;
+            
+            else if(matrix[row][col]<target){
+                row++;
+            }
+            else col--;
         }
         return false;
     }
