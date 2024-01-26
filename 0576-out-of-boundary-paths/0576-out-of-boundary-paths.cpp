11afd00cb233ec25bd75364b8f69d8122e1241aa
Riya <114819408+notdatriya@users.noreply.github.com>
2024-01-27 01:15:24 +0530

    Time: 9 ms (45.83%), Space: 10.4 MB (24.21%) - LeetHub

---
+++ 0576-out-of-boundary-paths/0576-out-of-boundary-paths.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int mod=1e9+7;
+    int solve(int x,int y,int m,int n,int maxmove,vector<vector<vector<int>>>&dp){
+        if(x<0 || x>m-1 || y<0 || y>n-1){
+            return 1;
+        }
+        
+        if(maxmove==0)return 0;
+        if(dp[x][y][maxmove]!=-1)return dp[x][y][maxmove];
+        
+        int ans=0;
+        ans+=solve(x,y-1,m,n,maxmove-1,dp)%mod;
+        ans %= mod;
+        ans+=solve(x,y+1,m,n,maxmove-1,dp)%mod;
+        ans %= mod;
+        ans+=solve(x-1,y,m,n,maxmove-1,dp)%mod;
+        ans %= mod;
+        ans+=solve(x+1,y,m,n,maxmove-1,dp)%mod;
+        ans %= mod;
+        
+        return dp[x][y][maxmove]=ans%mod;
+    }
+    int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
+        vector<vector<vector<int>>>dp(m,vector<vector<int>>(n,vector<int>(maxMove+1,-1)));
+        return solve(startRow,startColumn,m,n,maxMove,dp);
+    }
+};
