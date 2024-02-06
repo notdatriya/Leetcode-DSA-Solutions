1d737b9567baa7a5d04117e74acd4546aabe531a
Riya <114819408+notdatriya@users.noreply.github.com>
2024-02-06 23:24:52 +0530

    Time: 30 ms (60.89%), Space: 23.2 MB (35.48%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,23 +1,35 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        map<vector<int>,vector<string>>mpp;
-        for(auto w:strs){
-            vector<int>freq(26,0);
-            for(char c:w){
-                freq[c-'a']++;
-            }
-            mpp[freq].push_back(w);
+//         map<vector<int>,vector<string>>mpp;
+//         for(auto w:strs){
+//             vector<int>freq(26,0);
+//             for(char c:w){
+//                 freq[c-'a']++;
+//             }
+//             mpp[freq].push_back(w);
             
+//         }
+        
+//         vector<vector<string>>ans;
+//         for(auto k=mpp.begin();k!=mpp.end();k++){
+//             ans.push_back(k->second);
+//         }
+//         return ans;
+        
+        
+        map<string,vector<string>>mpp;
+        vector<vector<string>>ans;
+        for(int i=0;i<strs.size();i++){
+            string word=strs[i];
+            sort(word.begin(),word.end());
+            mpp[word].push_back(strs[i]);
         }
         
-        vector<vector<string>>ans;
         for(auto k=mpp.begin();k!=mpp.end();k++){
             ans.push_back(k->second);
         }
         return ans;
-        
-        
        
     }
 };
