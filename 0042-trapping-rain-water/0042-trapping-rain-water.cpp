03ca9bd7d0434c326323ca37842c087addae1ee2
Riya <114819408+notdatriya@users.noreply.github.com>
2024-02-06 22:16:47 +0530

    Time: 15 ms (31.29%), Space: 23 MB (9.87%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        
+        
+        //Brute force
+        
+//         int n=height.size();
+//         int watertrapped=0;
+//         for(int i=0;i<n;i++){
+//             int j=i;    
+//     int leftmax = 0, rightmax = 0;
+//             while(j>=0){
+//                 leftmax=max(leftmax,height[j]);
+//                 j--;
+//             }
+            
+//             j=i;
+            
+//             while(j<n){
+//                 rightmax=max(rightmax,height[j]);
+//                 j++;
+//             }
+            
+//             watertrapped+=min(leftmax,rightmax)-height[i];
+//         }
+//         return watertrapped;
+        
+        
+        //better solution
+        
+        int n=height.size();
+        vector<int>prefixmax(n);
+        vector<int>suffixmax(n);
+        
+        prefixmax[0]=height[0];
+        
+        for(int i=1;i<n;i++){
+            prefixmax[i]=max(prefixmax[i-1],height[i]);
+        }
+        
+        suffixmax[n-1]=height[n-1];
+        
+        for(int i=n-2;i>=0;i--){
+            suffixmax[i]=max(suffixmax[i+1],height[i]);
+        }
+        int waterTrapped = 0;
+  for (int i = 0; i < n; i++) {
+    waterTrapped += min(prefixmax[i], suffixmax[i]) - height[i];
+  }
+  return waterTrapped;
+        
+        
+        
+    }
+};
