4dae83de6858b81aa48ffd80949e09f9d3919a17
Riya <114819408+notdatriya@users.noreply.github.com>
2024-02-11 19:15:05 +0530

    Time: 85 ms (55.55%), Space: 19.4 MB (11.09%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -35,7 +35,53 @@ public:
         int n=grid.size();
         int m=grid[0].size();
         
-        vector<vector<vector<int>>>dp(n,vector<vector<int>>(m,vector<int>(m,-1)));
-        return solve(0,0,m-1,n,m,grid,dp);
+        // vector<vector<vector<int>>>dp(n,vector<vector<int>>(m,vector<int>(m,-1)));
+        // return solve(0,0,m-1,n,m,grid,dp);
+        
+        
+        
+        //tabulation
+    vector<vector<vector<int>>>dp(n,vector<vector<int>>(m,vector<int>(m,0)));
+        
+         for (int y1 = 0; y1 < m; y1++) {
+        for (int y2 = 0; y2 < m; y2++) {
+            if (y1 == y2)
+                dp[n - 1][y1][y2] = grid[n - 1][y1];
+            else
+                dp[n - 1][y1][y2] = grid[n - 1][y1] + grid[n - 1][y2];
+        }
+    }
+
+    for (int i = n - 2; i >= 0; i--) {
+        for (int y1 = 0; y1 < m; y1++) {
+            for (int y2 = 0; y2 < m; y2++) {
+                int maxi = INT_MIN;
+
+                // Inner nested loops to try out 9 options (diagonal moves)
+                for (int di = -1; di <= 1; di++) {
+                    for (int dj = -1; dj <= 1; dj++) {
+                        int ans;
+
+                        if (y1 == y2)
+                            ans = grid[i][y1];
+                        else
+                            ans = grid[i][y1] + grid[i][y2];
+
+                        if ((y1 + di < 0 || y1 + di >= m) || (y2 + dj < 0 || y2 + dj >= m))
+                            ans += -1e9; 
+                        else
+                            ans += dp[i + 1][y1 + di][y2 + dj]; 
+
+                        maxi = max(ans, maxi); 
+                    }
+                }
+                dp[i][y1][y2] = maxi; 
+            }
+        }
+    }
+
+    
+    return dp[0][0][m - 1];
+
     }
 };
