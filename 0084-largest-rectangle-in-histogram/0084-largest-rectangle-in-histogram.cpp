364680eeae6bef6ba90ed47c5a560347b1974239
Riya <114819408+notdatriya@users.noreply.github.com>
2024-02-13 12:03:06 +0530

    Time: 91 ms (98.01%), Space: 77.7 MB (76.84%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,49 +1,71 @@
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
-        int n=heights.size();
-      int sm[n];
-        int la[n];
-        stack<int>st;
+//         int n=heights.size();
+//       int sm[n];
+//         int la[n];
+//         stack<int>st;
         
-        for(int i=0;i<n;i++){
-                while(!st.empty() && heights[st.top()]>=heights[i]){
-                    st.pop();
-                }
-                if(st.empty()){
-                    sm[i]=0;
-                }
-                else{
-                    sm[i]=st.top()+1;
-                }
-                st.push(i);
+//         for(int i=0;i<n;i++){
+//                 while(!st.empty() && heights[st.top()]>=heights[i]){
+//                     st.pop();
+//                 }
+//                 if(st.empty()){
+//                     sm[i]=0;
+//                 }
+//                 else{
+//                     sm[i]=st.top()+1;
+//                 }
+//                 st.push(i);
             
-        }
+//         }
         
-        while(!st.empty())st.pop();
+//         while(!st.empty())st.pop();
         
-         for(int i=n-1;i>=0;i--){
-                while(!st.empty() && heights[st.top()]>=heights[i]){
-                    st.pop();
-                }
-                if(st.empty()){
-                    la[i]=n-1;
-                }
-                else{
-                    la[i]=st.top()-1;
-                }
-                st.push(i);
+//          for(int i=n-1;i>=0;i--){
+//                 while(!st.empty() && heights[st.top()]>=heights[i]){
+//                     st.pop();
+//                 }
+//                 if(st.empty()){
+//                     la[i]=n-1;
+//                 }
+//                 else{
+//                     la[i]=st.top()-1;
+//                 }
+//                 st.push(i);
              
-        }
-        int mul=0;
-        int maxmul=0;
-        for(int i=0;i<n;i++){
-            mul=(la[i]-sm[i]+1)*heights[i];
-            maxmul=max(mul,maxmul);
-        }
-        return maxmul;
+//         }
+//         int mul=0;
+//         int maxmul=0;
+//         for(int i=0;i<n;i++){
+//             mul=(la[i]-sm[i]+1)*heights[i];
+//             maxmul=max(mul,maxmul);
+//         }
+//         return maxmul;
         
         
+        
+        //m-2 using one pass
+        
+        stack<int>st;  //we will store index
+        int n=heights.size();
+        int maxarea=0;
+        
+        for(int i=0;i<=n;i++){
+            while(!st.empty() && (i==n || heights[st.top()]>=heights[i])){
+                int h=heights[st.top()];
+                st.pop();
+                int width;
+                if(st.empty())width=i;
+                else{
+                    width=i-st.top()-1;
+                }
+                
+                maxarea=max(maxarea,width*h);
+            }
+            st.push(i);
+        }
+        return maxarea;
         
         
       
