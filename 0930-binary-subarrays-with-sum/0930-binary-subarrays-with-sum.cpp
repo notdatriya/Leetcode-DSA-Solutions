a721654364fcd063fef70c4c3cd446a436551170
Riya <114819408+notdatriya@users.noreply.github.com>
2024-02-16 20:15:45 +0530

    Time: 49 ms (33.80%), Space: 38.2 MB (21.74%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
@@ -5,10 +5,10 @@ public:
          int n = nums.size();
         int count = 0;
         int sum = 0;
-        unordered_map<int, int> mpp;
-        mpp[0] = 1; 
+        unordered_map<int, int> mpp; 
         for (int i = 0; i < n; i++) {
             sum += nums[i];
+            if(sum==goal)count++;
             if (mpp.find(sum-goal) != mpp.end()) {
                 count += mpp[sum - goal];
             }
