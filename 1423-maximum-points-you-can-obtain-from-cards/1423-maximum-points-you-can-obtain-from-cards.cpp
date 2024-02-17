092c5131343e3a4552eaeab40d74e2b50815679a
Riya <114819408+notdatriya@users.noreply.github.com>
2024-02-17 14:42:58 +0530

    Time: 39 ms (83.52%), Space: 44.6 MB (43.32%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
@@ -2,27 +2,45 @@ class Solution {
 public:
     int maxScore(vector<int>& cardPoints, int k) {
         
-        int i=0;
-        int sum=0;
-        int minisum=INT_MAX;
-        int n=cardPoints.size();
-        int tsum=0;
+//         int i=0;
+//         int sum=0;
+//         int minisum=INT_MAX;
+//         int n=cardPoints.size();
+//         int tsum=0;
         
-        for(int j=0;j<n;j++){
-            sum+=cardPoints[j];
+//         for(int j=0;j<n;j++){
+//             sum+=cardPoints[j];
         
             
-            if(j-i+1>n-k){
-                sum-=cardPoints[i];
-                i++;
-            }
-             if(j-i+1==n-k){
-                minisum=min(minisum,sum);
-            }
+//             if(j-i+1>n-k){
+//                 sum-=cardPoints[i];
+//                 i++;
+//             }
+//              if(j-i+1==n-k){
+//                 minisum=min(minisum,sum);
+//             }
             
-            tsum+=cardPoints[j];
+//             tsum+=cardPoints[j];
+//         }
+//         cout<<minisum<<" "<<tsum<<endl;
+//         return tsum-minisum;
+        
+        
+        //m-2- second different approach, both are equally good
+        
+         int sum = 0;
+        int n = cardPoints.size();
+        for (int i = 0; i < k; i++) {
+            sum += cardPoints[i];
         }
-        cout<<minisum<<" "<<tsum<<endl;
-        return tsum-minisum;
+        
+        int maxSum = sum;
+        for (int i = k - 1, j = n - 1; i >= 0; i--, j--) {
+            sum -= cardPoints[i];
+            sum += cardPoints[j];
+            maxSum = max(maxSum, sum);
+        }
+        
+        return maxSum;
     }
 };
