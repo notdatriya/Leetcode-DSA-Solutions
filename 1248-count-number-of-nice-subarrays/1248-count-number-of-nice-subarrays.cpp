691334dcdb773d770b4c0d65fd8e155fcde14383
Riya <114819408+notdatriya@users.noreply.github.com>
2024-02-17 12:03:01 +0530

    Time: 91 ms (88.78%), Space: 70 MB (51.55%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
@@ -1,29 +1,54 @@
 class Solution {
 public:
-    int numberOfSubarrays(vector<int>& nums, int k) {
+    
+    
+    int atmost(vector<int>&nums,int k){
         int n=nums.size();
-        
-        for(int i=0;i<n;i++){
-            if(nums[i]%2==0){
-                nums[i]=0;
-            }
-            else{
-                nums[i]=1;
-            }
-        }
-        
-        unordered_map<int,int>mpp;
-        mpp[0]=1;
+        int i=0;
         int count=0;
-        int sum=0;
-        for(int i=0;i<n;i++){
-            sum+=nums[i];
-            if(mpp.find(sum-k)!=mpp.end()){
-                count+=mpp[sum-k];
+        for(int j=0;j<n;j++){
+            if(nums[j]%2==1)k--;
+            
+            while(k<0){
+                if(nums[i]%2==1){
+                    k++;
+                }
+                i++;
             }
-            mpp[sum]++;
+            
+            
+            count+=j-i+1;
         }
         return count;
+    }
+    int numberOfSubarrays(vector<int>& nums, int k) {
+//         int n=nums.size();
+        
+//         for(int i=0;i<n;i++){
+//             if(nums[i]%2==0){
+//                 nums[i]=0;
+//             }
+//             else{
+//                 nums[i]=1;
+//             }
+//         }
+        
+//         unordered_map<int,int>mpp;
+//         mpp[0]=1;
+//         int count=0;
+//         int sum=0;
+//         for(int i=0;i<n;i++){
+//             sum+=nums[i];
+//             if(mpp.find(sum-k)!=mpp.end()){
+//                 count+=mpp[sum-k];
+//             }
+//             mpp[sum]++;
+//         }
+//         return count;
+        
+        
+        //m-2 sliding window
+        return atmost(nums,k)-atmost(nums,k-1);
         
     }
 };
