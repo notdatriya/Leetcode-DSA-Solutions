4f68b6db195ca46932a03ce97b3fe029f178e676
Riya <114819408+notdatriya@users.noreply.github.com>
2024-03-02 22:56:49 +0530

    Time: 2 ms (37.67%), Space: 7.2 MB (70.42%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,9 +1,13 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
+        // if(n<=0)return false;
+        // int a=log2(n);
+        // double b=log2(n);
+        // return a==b;
+        
         if(n<=0)return false;
-        int a=log2(n);
-        double b=log2(n);
-        return a==b;
+        if((n& (n-1))==0)return true;
+        return false;
     }
 };
