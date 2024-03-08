7e1b29ef9c2dc11b94a1290b4bc0d34630acc43f
Riya <114819408+notdatriya@users.noreply.github.com>
2024-03-08 13:56:41 +0530

    Time: 7 ms (40.22%), Space: 22 MB (17.21%) - LeetHub

--- 3005-count-elements-with-maximum-frequency/3005-count-elements-with-maximum-frequency.cpp
+++ 3005-count-elements-with-maximum-frequency/3005-count-elements-with-maximum-frequency.cpp
@@ -2,19 +2,38 @@ class Solution {
 public:
     int maxFrequencyElements(vector<int>& nums) {
         
-        map<int,int>mpp;
+//         map<int,int>mpp;
+//         int maxi=0;
+//         for(int i=0;i<nums.size();i++){
+//             mpp[nums[i]]++;
+//             maxi=max(maxi,mpp[nums[i]]);
+//         }
+        
+//         int count=0;
+//         for(auto i:nums){
+//             if(mpp[i]==maxi){
+//                 count++;
+//             }
+//         }
+//         return count;
+        
+        
+        //m-2 one pass
+         map<int,int>mpp;
         int maxi=0;
+        int count=0;
         for(int i=0;i<nums.size();i++){
             mpp[nums[i]]++;
-            maxi=max(maxi,mpp[nums[i]]);
-        }
-        
-        int count=0;
-        for(auto i:nums){
-            if(mpp[i]==maxi){
-                count++;
+            int freq=mpp[nums[i]];
+            if(freq>maxi){
+                maxi=freq;
+                count=freq;
+            }
+            else if(freq==maxi){
+                count+=freq;
             }
         }
         return count;
+        
     }
 };
