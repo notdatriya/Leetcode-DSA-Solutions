8a4d7ba3e93bfeb59bad44f272687aae97e5cb65
Riya <114819408+notdatriya@users.noreply.github.com>
2024-03-15 10:14:00 +0530

    Time: 10 ms (16.05%), Space: 48.6 MB (8.84%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,32 +1,62 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        stack<string>st;
-         int n=s.length();                          
-        string ans;
-        string str;
-        // int i=0;
-        for(int i=0;i<n;i++){
-            if(s[i]!=' '){  
-            str+=s[i];
-            }
-            if((s[i+1]==' '&& !str.empty()) || (i==n-1 && s[i]!=' ')){
-                st.push(str);
-                cout<<st.top();
-                str="";
-            }
+//         stack<string>st;
+//          int n=s.length();                          
+//         string ans;
+//         string str;
+//         // int i=0;
+//         for(int i=0;i<n;i++){
+//             if(s[i]!=' '){  
+//             str+=s[i];
+//             }
+//             if((s[i+1]==' '&& !str.empty()) || (i==n-1 && s[i]!=' ')){
+//                 st.push(str);
+//                 cout<<st.top();
+//                 str="";
+//             }
              
-        }
+//         }
         
-        while(st.size()>1){
-            ans+=st.top();
-            ans+=" ";
-            st.pop();
-        }
-        ans+=st.top();
-        st.pop();
+//         while(st.size()>1){
+//             ans+=st.top();
+//             ans+=" ";
+//             st.pop();
+//         }
+//         ans+=st.top();
+//         st.pop();
         
+//         return ans;
+        
+        
+        
+        //m-2 two pointer approach
+        
+        int n = s.size();
+        string ans = "";
+            int i=n-1;
+        while(i>=0) {
+
+            if (s[i] != ' ') {
+
+                int end = i;
+                while (i >= 0 && s[i] != ' ') {
+                    i--;
+                }
+                int start = i + 1;
+
+                if (ans == "")
+                    ans = ans + s.substr(start, end - start + 1);
+                else
+                    ans = ans + " " + s.substr(start, end - start + 1);
+            }
+            
+            i--;
+        }
+
         return ans;
+        
+        
         
     }
 };
