2edf5887bc893888c3ce537d3b2b725aa9a35471
Riya <114819408+notdatriya@users.noreply.github.com>
2024-04-09 00:31:25 +0530

    Time: 5 ms (31.29%), Space: 10.6 MB (99.56%) - LeetHub

--- 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.cpp
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.cpp
@@ -1,35 +1,61 @@
 class Solution {
 public:
     int countStudents(vector<int>& students, vector<int>& sandwiches) {
-      queue<int> q;
-                stack<int>st;
+//       queue<int> q;
+//                 stack<int>st;
 
-        int n=students.size();
-        for (int i=0;i<n;i++) {
+//         int n=students.size();
+//         for (int i=0;i<n;i++) {
             
-            st.push(sandwiches[n-i-1]);
-            q.push(students[i]);
-        }
+//             st.push(sandwiches[n-i-1]);
+//             q.push(students[i]);
+//         }
         
         
-        int lastserved=0;
+//         int lastserved=0;
         
-        while(!q.empty() && lastserved<q.size()){
-            int stu=q.front();
-            int sand=st.top();
-            if(stu==sand){
-                st.pop();
-                q.pop();
-                lastserved=0;
+//         while(!q.empty() && lastserved<q.size()){
+//             int stu=q.front();
+//             int sand=st.top();
+//             if(stu==sand){
+//                 st.pop();
+//                 q.pop();
+//                 lastserved=0;
                 
-            }
-            else{
-                q.push(stu);
-                q.pop();
-                lastserved++;
-            }
+//             }
+//             else{
+//                 q.push(stu);
+//                 q.pop();
+//                 lastserved++;
+//             }
+//         }
+        
+//         return q.size();
+        
+        
+        
+        
+        //m-2 countingggg
+        
+        
+        int ccount=0;
+        int scount=0;
+        
+        for(int it:students){
+            if(it==0)ccount++;
+            else scount++;
         }
         
-        return q.size();
+        
+        for(int s:sandwiches){
+            if(s==0 && ccount==0)return scount;
+            if(s==1 && scount==0)return ccount;
+            
+            if(s==0)ccount--;
+            else{
+                scount--;
+            }
+        }
+        return 0;
     }
 };
