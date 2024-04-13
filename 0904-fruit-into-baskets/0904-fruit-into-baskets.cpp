9a233447856dfd46e1570b0497ee30e94b8b5eb9
Riya <114819408+notdatriya@users.noreply.github.com>
2024-04-14 01:34:17 +0530

    Time: 129 ms (36.05%), Space: 77.6 MB (8.45%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -3,26 +3,57 @@ public:
     int totalFruit(vector<int>& fruits) {
         
         
-        int i=0;
-        int n=fruits.size();
-        int maxcount=0;
-        map<int,int>basket;
-        for(int j=0;j<n;j++){
-        basket[fruits[j]]++;
+//         int i=0;
+//         int n=fruits.size();
+//         int maxcount=0;
+//         map<int,int>basket;
+        
+//         for(int j=0;j<n;j++){
             
-            while(basket.size()>2){
-                int count=basket[fruits[i]];
-                if(count==1){
-                    basket.erase(fruits[i]);
+//         basket[fruits[j]]++;
+            
+//             while(basket.size()>2){
+//                 int count=basket[fruits[i]];
+//                 if(count==1){
+//                     basket.erase(fruits[i]);
+//                 }
+//                 else{
+//                     basket[fruits[i]]--;
+//                 }
+//                 i++;
+//             }
+            
+//             maxcount=max(maxcount,j-i+1);
+//         }
+//         return maxcount;
+        
+        
+        
+        
+        int l=0;
+        int r=0;
+        int n=fruits.size();
+        map<int,int>mpp;
+        int maxi=0;
+        while(r<n){
+            mpp[fruits[r]]++;
+            
+            if(mpp.size()>2){
+                while(mpp.size()>2){
+                    mpp[fruits[l]]--;
+                    if(mpp[fruits[l]]==0){
+                        mpp.erase(fruits[l]);
+                    }
+                    l++;
                 }
-                else{
-                    basket[fruits[i]]--;
-                }
-                i++;
             }
             
-            maxcount=max(maxcount,j-i+1);
+            if(mpp.size()<=2){
+                maxi=max(maxi,r-l+1);
+            }
+            r++;
         }
-        return maxcount;
+        
+        return maxi;
     }
 };
