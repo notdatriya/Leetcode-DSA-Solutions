f45ab393f0be9c2ee068b40ed239620022421b6d
Riya <114819408+notdatriya@users.noreply.github.com>
2024-04-15 01:06:27 +0530

    Time: 20 ms (55.84%), Space: 9.6 MB (91.44%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
@@ -2,20 +2,37 @@ class Solution {
 public:
     int numberOfSubstrings(string s) {
         
-        int n=s.size();
+//         int n=s.size();
         
-        int i=0;
-        unordered_map<char,int>mpp;
-        int count=0;
-        for(int j=0;j<n;j++){
-            mpp[s[j]]++;
+//         int i=0;
+//         unordered_map<char,int>mpp;
+//         int count=0;
+//         for(int j=0;j<n;j++){
+//             mpp[s[j]]++;
             
-            while(mpp['a']>=1 && mpp['b']>=1 && mpp['c']>=1){
-                count+=(n-1)-j+1;
-                mpp[s[i]]--;
-                i++;
+//             while(mpp['a']>=1 && mpp['b']>=1 && mpp['c']>=1){
+//                 count+=(n-1)-j+1;
+//                 mpp[s[i]]--;
+//                 i++;
+//             }
+//         }
+//         return count;
+        
+        
+        //m-2 considering substrings ending with characters
+        
+        int n=s.size();
+        int lastseen[3]={-1,-1,-1};
+        int count=0;
+        
+        for(int i=0;i<n;i++){
+            lastseen[s[i]-'a']=i;
+            
+            if(lastseen[0]!=-1 && lastseen[0]!=-1 && lastseen[0]!=-1){
+                count+=(1+ min(lastseen[0],min(lastseen[1],lastseen[2])));
             }
         }
+        
         return count;
         
     }
