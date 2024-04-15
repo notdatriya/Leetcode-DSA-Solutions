0976bc43a9208e7ef156e545ae861af24fe5ef78
Riya <114819408+notdatriya@users.noreply.github.com>
2024-04-16 01:37:59 +0530

    Time: 15 ms (95.83%), Space: 30.9 MB (98.82%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
@@ -1,20 +1,43 @@
 class Solution {
 public:
     
+    int solve(vector<int>&nums,int goal){
+        if(goal<0)return 0;
+        int l=0;
+        int r=0;
+        int sum=0;
+        int count=0;
+        int n=nums.size();
+        while(r<n){
+            sum+=nums[r];
+            
+            while(sum>goal){
+                sum-=nums[l];
+                l++;
+            }
+            count+=(r-l+1);
+            r++;
+        }
+        return count;
+    }
+    
     int numSubarraysWithSum(vector<int>& nums, int goal) {
         
-         int n = nums.size();
-        int count = 0;
-        int sum = 0;
-        unordered_map<int, int> mpp; 
-        mpp[0]=1;
-        for (int i = 0; i < n; i++) {
-            sum += nums[i];
-            if (mpp.find(sum-goal) != mpp.end()) {
-                count += mpp[sum - goal];
-            }
-            mpp[sum]++;
-        }
-        return count;        
+        //  int n = nums.size();
+        // int count = 0;
+        // int sum = 0;
+        // unordered_map<int, int> mpp; 
+        // mpp[0]=1;
+        // for (int i = 0; i < n; i++) {
+        //     sum += nums[i];
+        //     if (mpp.find(sum-goal) != mpp.end()) {
+        //         count += mpp[sum - goal];
+        //     }
+        //     mpp[sum]++;
+        // }
+        // return count;  
+        
+        
+        return solve(nums,goal)-solve(nums,goal-1);
     }
 };
