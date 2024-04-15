35da1e9fd8fa7ba3a9506360def6e1d4f3e6776b
Riya <114819408+notdatriya@users.noreply.github.com>
2024-04-16 00:46:04 +0530

    Time: 8 ms (64.54%), Space: 8.1 MB (82.20%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -24,31 +24,58 @@ public:
         
         
         
-        int maxlen=0;
-        int n=s.length();
+//         int maxlen=0;
+//         int n=s.length();
         
-        for(int i=0;i<n;i++){
-            int hash[26]={0};
-            int maxf=0;
-            for(int j=i;j<n;j++){
-                hash[s[j]-'A']++;
+//         for(int i=0;i<n;i++){
+//             int hash[26]={0};
+//             int maxf=0;
+//             for(int j=i;j<n;j++){
+//                 hash[s[j]-'A']++;
                 
-                maxf=max(maxf,hash[s[j]-'A']);
-                int changes=(j-i+1)-maxf;
+//                 maxf=max(maxf,hash[s[j]-'A']);
+//                 int changes=(j-i+1)-maxf;
                 
-                if(changes<=k){
-                    maxlen=max(maxlen,j-i+1);
-                }
-                else{
-                    break;
-                }
-            }
-        }
-        return maxlen;
+//                 if(changes<=k){
+//                     maxlen=max(maxlen,j-i+1);
+//                 }
+//                 else{
+//                     break;
+//                 }
+//             }
+//         }
+//         return maxlen;
         
         
         
         //optimal
+        
+        int l=0;
+        int r=0;
+        int maxlen=0;
+        int maxf=0;
+        int n=s.length();
+        int hash[26]={0};
+        
+        while(r<n){
+            hash[s[r]-'A']++;
+            maxf=max(maxf,hash[s[r]-'A']);
+            
+            while(((r-l+1)-maxf)>k){
+                hash[s[l]-'A']--;
+                maxf=0;
+                for(int i=0;i<26;i++){
+                    maxf=max(maxf,hash[i]);
+                }
+                l++;
+            }
+            
+            if(((r-l+1)-maxf)<=k){
+                maxlen=max(maxlen,(r-l+1));
+            }
+            r++;
+        }
+        return maxlen;
         
         
     }
