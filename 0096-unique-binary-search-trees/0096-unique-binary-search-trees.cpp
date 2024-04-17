b78bda123a5cb8247ea1fcfea658c3b29eb0138b
Riya <114819408+notdatriya@users.noreply.github.com>
2024-04-17 22:49:25 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (49.99%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -19,10 +19,30 @@ public:
          if(n<=1)return 1;
         if(dp[n]!=-1)return dp[n];
         int ans=0;
+        
+        //i as root node
         for(int i=1;i<=n;i++){
             ans+=solve(i-1,dp)*solve(n-i,dp);
         }
         return dp[n]=ans;
+    }
+    
+    
+    
+    //bottom up solution
+    
+    int solveTab(int n){
+        vector<int>dp(n+1,0);
+        
+        dp[0]=dp[1]=1;
+        
+        for(int i=2;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                dp[i]+=dp[j-1]*dp[i-j];
+            }
+        }
+        
+        return dp[n];
     }
    
     int numTrees(int n) {
@@ -34,8 +54,12 @@ public:
 //         }
 //         return ans;
         
-        vector<int>dp(n+1,-1);
-        return solve(n,dp);
+        // vector<int>dp(n+1,-1);
+        // return solve(n,dp);
+        
+        
+        //tabulation
+        return solveTab(n);
         
         
         
