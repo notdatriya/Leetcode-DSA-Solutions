e1896e06031b335adb0c39f3b79cdd232bd33bea
Riya <114819408+notdatriya@users.noreply.github.com>
2024-04-17 22:35:57 +0530

    Time: 2023 ms (12.79%), Space: 7.3 MB (18.02%) - LeetHub

---
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -0,0 +1,28 @@
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+    
+    Node(int val){
+        data=val;
+        left=NULL;
+        right=NULL;
+    }
+};
+
+
+class Solution {
+public:
+   
+    int numTrees(int n) {
+       if(n<=1)return 1;
+        
+        int ans=0;
+        for(int i=1;i<=n;i++){
+            ans+=numTrees(i-1)*numTrees(n-i);
+        }
+        return ans;
+        
+    }
+};
