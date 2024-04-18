a9deae602e9305f9151ff8e96f847c2b6f7be31b
Riya <114819408+notdatriya@users.noreply.github.com>
2024-04-18 14:04:20 +0530

    Time: 78 ms (44.92%), Space: 101.4 MB (26.99%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.cpp
+++ 0463-island-perimeter/0463-island-perimeter.cpp
@@ -1,20 +1,24 @@
 class Solution {
 public:
     
-    void solve(int r,int c,vector<vector<int>>&grid,int n,int m,int &count, vector<vector<int>>&vis){
+    int solve(int r,int c,vector<vector<int>>&grid,int n,int m){  //int &count
         
         if(r<0 || r>n-1 || c<0 || c>m-1 || grid[r][c]==0){
-            count++;
-            return;
+            // count++;
+            return 1;
         }
         
-        if(vis[r][c]==1)return;
-     
+        // if(vis[r][c]==1)return;
         
-        vis[r][c]=1;
+        if(grid[r][c]==-1){
+            return 0;
+        }
+        
+        grid[r][c]=-1;
         int dr[4]={-1,0,0,1};
         int dc[4]={0,1,-1,0};
         
+        int count=0;
         
         for(int i=0;i<4;i++){
             
@@ -22,8 +26,9 @@ public:
             int nc=c+dc[i];
         
             
-            solve(nr,nc,grid,n,m,count,vis);
+            count+=solve(nr,nc,grid,n,m);
         }
+        return count;
         
         
     }
@@ -32,16 +37,17 @@ public:
         int m=grid[0].size();
         int count=0;
         
-        vector<vector<int>>vis(n,vector<int>(m,0));
+        // vector<vector<int>>vis(n,vector<int>(m,0));
         
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(!vis[i][j] && grid[i][j]==1){
-                    solve(i,j,grid,n,m,count,vis);
+                if(grid[i][j]==1){   //can also be !vis[i][j]
+                    count+= solve(i,j,grid,n,m);
                 }
             }
         }
-        return count;
+         return count;
+        
         
     }
 };
